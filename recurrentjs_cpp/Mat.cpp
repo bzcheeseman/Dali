a72241d99c6e3f65548995bf3d1485e003666a46
Jonathan Raiman <jonathanraiman@gmail.com>
2015-01-15 12:24:17 -0500

    added graph and limited backpropagation to C++ implementation

--- recurrentjs_cpp/Mat.cpp
+++ recurrentjs_cpp/Mat.cpp
@@ -1,92 +1,180 @@
-#include <iostream>
-#include <iomanip>
-#include <random>
-
-
-typedef double REAL_t;
-
-// Utilities:
-
-void fill_random(int n, REAL_t std, REAL_t * ptr) {
-	REAL_t * end = ptr + n;
-	std::default_random_engine generator;
-	std::normal_distribution<REAL_t> distribution(0.0,std);
-	while (ptr != end) {
-		*ptr = distribution(generator);
-		ptr++;
-	}
-}
-
-void print_matrix(int n, int d, REAL_t * ptr) {
-	REAL_t * end = ptr + n * d;
-	int i = 0;
-	std::cout << "[";
-	while (ptr != end ) {
-		std::cout << std::fixed
-				  << std::setw( 7 )
-				  << std::setprecision( 3 )
-				  << std::setfill( ' ' )
-				  << *ptr;
-		i++;
-		if (i == n * d) {
-			std::cout << "]" << std::endl;
-		} else {
-			std::cout << " ";
-		}
-		if (i % d == 0 && i < n * d) {
-			std::cout << "\n ";
-		}
-		ptr++;
-	}
-}
-
+#include "utils.hpp"
 // Matrix class (with random initializer):
 
-class Mat {
-	int n;
-	int d;
-	REAL_t * w;
-	REAL_t * dw;
+template<typename T> class Mat {
+	public:
+		int n;
+		int d;
+		T * w;
+		T * dw;
+
+		Mat (int n, int d) {
+			this->n = n;
+			this->d = d;
+			this->w = (T *) calloc(n * d, sizeof(T));
+			this->dw = (T *) calloc(n * d, sizeof(T));
+		}
+		Mat (int n, int d, T* w) {
+			this->n = n;
+			this->d = d;
+			this->w = w;
+			this->dw = (T *) calloc(n * d, sizeof(T));
+		}
+		void print () {utils::print_matrix(this->n, this->d, this->w);}
+		~Mat() {
+			free(this->w);
+			free(this->dw);
+		}
+		// random matrices:
+		Mat (int n, int d, T std) {
+			this->n = n;
+			this->d = d;
+			this->w = (T *) malloc(n * d * sizeof(T));
+			utils::fill_random(n * d, std, this->w);
+			this->dw = (T *) calloc(n * d, sizeof(T));
+		}
+		static Mat RandMat(int n, int d, T std) {
+			return Mat(n, d, std);
+		}
+
+		friend std::ostream& operator<<(std::ostream& strm, const Mat& a) {
+			return strm << "<#Mat n=" << a.n << ", d=" << a.d << " >";
+		}
+		operator std::string() const {
+			return "<#Mat n=" << this->n << ", d=" << this->d << " >";
+		}
+};
+
+
+
+template<typename T> class Backward {
+	Mat<T> * matrix1;
+	Mat<T> * matrix2;
+	Mat<T> * out;
+	uint type;
+	public:
+
+		Backward (Mat<T> * matrix1, Mat<T> * out, uint type) {
+			this->matrix1 = matrix1;
+			this->out = out;
+			this->type = type;
+		}
+		Backward (Mat<T> * matrix1, Mat<T> * matrix2, Mat<T> * out, uint type) {
+			this->matrix1 = matrix1;
+			this->matrix2 = matrix2;
+			this->out = out;
+			this->type = type;
+		}
+
+		friend std::ostream& operator<<(std::ostream& strm, const Backward<T>& a) {
+			return strm << "<#Backward matrix = " << *(a.matrix1) << ", out = " << *(a.out) << ">";
+		}
+		operator std::string() const {
+			return "<#Backward matrix = " << *(this->matrix1) << ", out = " << *(this->out) << ">";
+		}
+};
+
+template<typename T> class Graph {
+	bool needs_backprop;
+	std::vector<Backward<T> > backprop;
 
 	public:
 
-	Mat (int n, int d) {
-		this->n = n;
-		this->d = d;
-		this->w = (REAL_t *) calloc(n * d, sizeof(REAL_t));
-		this->dw = (REAL_t *) calloc(n * d, sizeof(REAL_t));
-	}
+		Graph (bool needs_backprop) {
+			this->needs_backprop = needs_backprop;
+		}
+		Graph () {
+			this->needs_backprop = true;
+		}
+		void backward () {
+			// std::vector<int> myvector;
+			// for (int i=1; i<=5; i++) myvector.push_back(i);
+			std::cout << "myvector contains (reverse):";
+			for (typename std::vector<Backward<T> >::reverse_iterator it = this->backprop.rbegin(); it != this->backprop.rend(); ++it) {
+				std::cout << ' ' << *it;
+			}
+			std::cout << '\n';
+		}
+		Mat<T>* eltmul(Mat<T>& matrix1, Mat<T>& matrix2) {
+			if (matrix1.n != matrix2.n || matrix1.d != matrix2.d) {
+				throw std::invalid_argument("Matrices cannot be element-wise multiplied, they do not have the same dimensions.");
+			}
+			Mat<T>* out = new Mat<T>(
+				matrix1.n,
+				matrix1.d,
+				utils::element_mult(
+					matrix1.n * matrix1.d,
+					matrix1.w,
+					matrix2.w)
+				);
 
-	Mat (int n, int d, REAL_t std) {
-		this->n = n;
-		this->d = d;
-		this->w = (REAL_t *) malloc(n * d * sizeof(REAL_t));
-		fill_random(n * d, std, this->w);
-		this->dw = (REAL_t *) calloc(n * d, sizeof(REAL_t));
-	}
+			if (this->needs_backprop) {
+				// allocates a new backward element in the vector using these arguments:
+				this->backprop.emplace_back(&matrix1, &matrix2, out, utils::ops::eltmul);
+			}
+		}
+		Mat<T>* add(Mat<T>& matrix1, Mat<T>& matrix2) {
+			if (matrix1.n != matrix2.n || matrix1.d != matrix2.d) {
+				throw std::invalid_argument("Matrices cannot be added, they do not have the same dimensions.");
+			}
+			Mat<T>* out = new Mat<T>(
+				matrix1.n,
+				matrix1.d,
+				utils::element_sum(
+					matrix1.n * matrix1.d,
+					matrix1.w,
+					matrix2.w)
+				);
 
-	void print () {
-		print_matrix(this->n, this->d, this->w);
-	}
+			if (this->needs_backprop) {
+				// allocates a new backward element in the vector using these arguments:
+				this->backprop.emplace_back(&matrix1, out, utils::ops::add);
+				this->backprop.emplace_back(&matrix2, out, utils::ops::add);
+			}
 
-	~Mat() {
-		free(this->w);
-		free(this->dw);
-	}
+			return out;
+		}
 
-	static Mat RandMat(int n, int d, REAL_t std) {
-		return Mat(n, d, std);
-	}
+		Mat<T>* mul(Mat<T>& matrix1, Mat<T>& matrix2) {
+			if (matrix1.d != matrix2.n) {
+				throw std::invalid_argument("matmul dimensions misaligned.");
+			}
+			// TODO: use eigen for these matrix operations:
+			Mat<T>* out = new Mat<T>(
+				matrix1.n,
+				matrix1.d,
+				utils::matrix_dot_product(
+					matrix1.n * matrix1.d,
+					matrix1.w,
+					matrix2.w)
+				);
+
+			if (this->needs_backprop) {
+				// allocates a new backward element in the vector using these arguments:
+				this->backprop.emplace_back(&matrix1, &matrix2, out, utils::ops::mul);
+			}
+
+			return out;
+		}
 };
 
-int main()
-{
+typedef double REAL_t;
 
-    Mat mymatrix(3,5);
-
-    Mat mymatrix2 = Mat::RandMat(3, 4, 2.0);
-
+int main() {
+	// build blank matrix of double type:
+    Mat<REAL_t> mymatrix(3,5);
+    // build random matrix of REAL_t type with standard deviation 2:
+    Mat<REAL_t> mymatrix2 = Mat<REAL_t>::RandMat(3, 5, 2.0);
     mymatrix2.print();
+    mymatrix.print();
+
+    Graph<REAL_t> mygraph;
+
+    std::unique_ptr< Mat<REAL_t> > thesum( mygraph.add(mymatrix, mymatrix2) );
+
+    thesum->print();
+
+    mygraph.backward();
 
     return 0;
 }
---
+++ recurrentjs_cpp/utils.hpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <iomanip>
+#include <random>
+
+namespace utils
+{
+	template<typename T> void print_matrix(int n, int d, T * ptr) {
+		T * end = ptr + n * d;
+		int i = 0;
+		std::cout << "["; // print starting bracket
+		while (ptr != end) {
+			std::cout << std::fixed
+					  << std::setw( 7 ) // keep 7 digits
+					  << std::setprecision( 3 ) // use 3 decimals
+					  << std::setfill( ' ' ) // pad values with blanks
+					  << *ptr;
+			i++;
+			if (i == n * d) {
+				std::cout << "]" << std::endl; // end with bracket
+			} else {
+				std::cout << " "; // or add a space between numbers
+			}
+			if (i % d == 0 && i < n * d) {
+				std::cout << "\n "; // skip a line between rows
+			}
+			ptr++;
+		}
+	}
+	template<typename T> void fill_random(int n, T std, T * ptr) {
+		std::default_random_engine generator;
+		std::random_device rd;
+		generator.seed(rd());
+		std::normal_distribution<T> distribution(0.0,std);
+		for (T * end = ptr + n; ptr != end;ptr++) *ptr = distribution(generator);
+	}
+
+	template<typename T> T* element_sum(int n, T* ptr1, T* ptr2) {
+		T* out_array = (T * ) malloc(n * sizeof(T));
+		T* iter = out_array;
+		for (T* end = ptr1 + n; ptr1 != end; ptr1++, ptr2++, iter++) *iter = (*ptr1) + (*ptr2);
+		return out_array;
+	}
+	template<typename T> T* element_mult(int n, T* ptr1, T* ptr2) {
+		T* out_array = (T * ) malloc(n * sizeof(T));
+		T* iter = out_array;
+		for (T* end = ptr1 + n; ptr1 != end; ptr1++, ptr2++, iter++) *iter = (*ptr1) * (*ptr2);
+		return out_array;
+	}
+	namespace ops {
+		static const uint add     = 0;
+		static const uint eltmul  = 1;
+		static const uint sigmoid = 2;
+		static const uint tanh    = 3;
+		static const uint mul     = 4;
+	}
+}

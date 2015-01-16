f4eea3c5b5a9b36bd87cca8daa4ee17cc575de94
Jonathan Raiman <jonathanraiman@gmail.com>
2015-01-15 21:56:14 -0500

    reduced usage of shared_ptr, visually at least

--- recurrentjs_cpp/Mat.cpp
+++ recurrentjs_cpp/Mat.cpp
@@ -316,39 +316,39 @@ void forward_model(Graph<T>& graph,
 int main() {
 	// build blank matrix of double type:
     std::shared_ptr< Mat<REAL_t>> A(new Mat<REAL_t>(3, 5) );
-    A->w = (A->w.array() + 1.2).matrix();
-
     // build random matrix of double type with standard deviation 2:
     std::shared_ptr< Mat<REAL_t> > B(new Mat<REAL_t>(A->n, A->d, 2.0));
     std::shared_ptr< Mat<REAL_t> > C(new Mat<REAL_t>(A->d, 4, 2.0));
+
+    A->w = (A->w.array() + 1.2).matrix();
 
     A->print();
     B->print();
 
     Graph<REAL_t> graph;
-	std::shared_ptr< Mat<REAL_t> > A_plus_B     = graph.add(A, B);
-	std::shared_ptr< Mat<REAL_t> > A_times_B    = graph.eltmul(A, B);
-	std::shared_ptr< Mat<REAL_t> > A_plus_B_sig = graph.sigmoid(A_plus_B);
-	std::shared_ptr< Mat<REAL_t> > A_dot_C      = graph.mul(A, C);
+	auto A_plus_B     = graph.add(A, B);
+	auto A_times_B    = graph.eltmul(A, B);
+	auto A_plus_B_sig = graph.sigmoid(A_plus_B);
+	auto A_dot_C      = graph.mul(A, C);
 
-    std::shared_ptr< Mat<REAL_t> > A_dot_C_tanh( graph.tanh(A_dot_C) );
+    auto A_dot_C_tanh = graph.tanh(A_dot_C);
 
     A_plus_B    ->print();
     A_times_B   ->print();
     A_plus_B_sig->print();
     A_dot_C     ->print();
 
-    std::shared_ptr< Mat<REAL_t> > A_plucked(graph.row_pluck(A, 2));
+    auto A_plucked = graph.row_pluck(A, 2);
     A_plucked->print();
     forward_model(graph, A, C);
 
-    std::shared_ptr< Mat<REAL_t> > prod(graph.mul(A, C));
-	std::shared_ptr< Mat<REAL_t> > activ(graph.tanh(prod));
+    auto prod = graph.mul(A, C);
+	auto activ = graph.tanh(prod);
 
     // add some random singularity and use exponential
     // normalization:
     A_plucked->w(2,0) += 3.0;
-    std::shared_ptr< Mat<REAL_t> > A_plucked_normed(softmax(A_plucked));
+    auto A_plucked_normed = softmax(A_plucked);
     A_plucked_normed->print();
 
     // backpropagate to A and B

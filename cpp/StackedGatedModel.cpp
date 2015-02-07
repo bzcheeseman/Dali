53fd8eb31f409b3d09f7a59d047709de6ec5e985
Jonathan Raiman <jonathanraiman@gmail.com>
2015-02-08 00:11:26 +0100

    added CLI creation options and parser, added clean masked error code

--- cpp/StackedGatedModel.cpp
+++ cpp/StackedGatedModel.cpp
@@ -1,13 +1,12 @@
 #include "StackedGatedModel.h"
 
-#include <iostream>
-#include <fstream>
-
 using std::shared_ptr;
 using std::vector;
 using std::make_shared;
 using std::ofstream;
 using std::to_string;
+using std::stringstream;
+using std::string;
 
 /**
 Parameters
@@ -101,6 +100,68 @@ void StackedGatedModel<T>::save(std::string dirname) {
 	save_configuration(dirname);
 }
 
+template<typename T>
+void StackedGatedModel<T>::add_options_to_CLI(optparse::OptionParser& parser) {
+	parser.set_defaults("stack_size", "4");
+	parser
+		.add_option("-stack", "--stack_size")
+		.help("How many LSTMs should I stack ?").metavar("INT");
+	parser.set_defaults("input_size", "100");
+	parser
+		.add_option("-i", "--input_size")
+		.help("Size of the word vectors").metavar("INT");
+	parser.set_defaults("hidden", "100");
+	parser
+		.add_option("-h", "--hidden")
+		.help("How many Cells and Hidden Units should each LSTM have ?").metavar("INT");
+	parser.set_defaults("decay_rate", "0.95");
+	parser
+		.add_option("-decay", "--decay_rate")
+		.help("What decay rate should RMSProp use ?").metavar("FLOAT");
+	parser.set_defaults("rho", "0.95");
+	parser
+		.add_option("--rho")
+		.help("What rho / learning rate should the Solver use ?").metavar("FLOAT");
+	parser.set_defaults("memory_penalty", "0.3");
+	parser
+		.add_option("--memory_penalty")
+		.help("L1 Penalty on Input Gate activation.").metavar("FLOAT");
+
+	parser.set_defaults("save", "");
+	parser.add_option("--save")
+		.help("Where to save the model to ?").metavar("FOLDER");
+
+	parser.set_defaults("load", "");
+	parser.add_option("--load")
+		.help("Where to load the model from ?").metavar("FOLDER");
+}
+
+template<typename T>
+StackedGatedModel<T> StackedGatedModel<T>::build_from_CLI(optparse::Values& options, int vocab_size, int output_size, bool verbose) {
+	using utils::from_string;
+	string load_location = options["load"];
+	if (verbose)
+		std::cout << "Load location         = " << ((load_location == "") ? "N/A" : load_location)       << std::endl;
+	// Load or Construct the model
+	auto model = (load_location != "") ?
+		StackedGatedModel<T>::load(load_location) :
+		StackedGatedModel<T>(
+			vocab_size,
+			from_string<int>(options["input_size"]),
+			from_string<int>(options["hidden"]),
+			from_string<int>(options["stack_size"]) < 1 ? 1 : from_string<int>(options["stack_size"]),
+			output_size,
+			from_string<T>(options["memory_penalty"]));
+	if (verbose) {
+		std::cout << ((load_location == "") ? "Constructed Stacked LSTMs" : "Loaded Model") << std::endl;
+		std::cout << "Vocabulary size       = " << model.embedding->n      << std::endl;
+		std::cout << "Input size            = " << model.input_size        << std::endl;
+		std::cout << "Output size           = " << model.output_size       << std::endl;
+		std::cout << "Stack size            = " << model.stack_size        << std::endl;
+		std::cout << "Memory Penalty        = " << model.memory_penalty    << std::endl;
+	}
+	return model;
+}
 /**
 Load
 ----
@@ -148,9 +209,10 @@ StackedGatedModel<T> StackedGatedModel<T>::load(std::string dirname) {
 }
 
 template<typename T>
-std::tuple<T, T> StackedGatedModel<T>::cost_fun(
+std::tuple<T, T> StackedGatedModel<T>::masked_predict_cost(
 	graph_t& G,
 	shared_index_mat data,
+	shared_index_mat target_data,
 	shared_eigen_index_vector start_loss,
 	shared_eigen_index_vector codelens,
 	uint offset) {
@@ -169,7 +231,6 @@ std::tuple<T, T> StackedGatedModel<T>::cost_fun(
 		// pick this letter from the embedding
 		input_vector = G.rows_pluck(embedding, data->col(i));
 		memory = gate.activate(G, input_vector, initial_state.second[0]);
-		memory->set_name("Memory Gated Input");
 		input_vector = G.eltmul_broadcast_rowwise(input_vector, memory);
 		// pass this letter to the LSTM for processing
 		initial_state = forward_LSTMs(G, input_vector, initial_state, cells);
@@ -180,8 +241,7 @@ std::tuple<T, T> StackedGatedModel<T>::cost_fun(
 			i,
 			start_loss,
 			codelens,
-			(data->col(i+1).array() - offset).matrix());
-
+			(target_data->col(i+1).array() - offset).matrix());
 		std::get<1>(cost) += masked_sum(memory, i, 0, start_loss, memory_penalty);
 	}
 	return cost;
@@ -281,9 +341,8 @@ StackedGatedModel<T>::StackedGatedModel (int _vocabulary_size, int _input_size, 
 // Nested Templates !!
 template<typename T>
 template<typename K>
-std::vector<int> StackedGatedModel<T>::reconstruct_fun(
+std::vector<int> StackedGatedModel<T>::reconstruct(
     K example,
-    utils::Vocab& lookup_table,
     int eval_steps,
     int symbol_offset) {
 
@@ -317,23 +376,138 @@ std::vector<int> StackedGatedModel<T>::reconstruct_fun(
 	return outputs;
 }
 
+template<typename T>
+template<typename K>
+std::vector<utils::OntologyBranch::shared_branch> StackedGatedModel<T>::reconstruct_lattice(
+    K example,
+    utils::OntologyBranch::shared_branch root,
+    int eval_steps) {
+
+	graph_t G(false);
+	shared_mat input_vector;
+	shared_mat memory;
+	auto pos = root;
+	auto initial_state = lstm::initial_states(hidden_sizes);
+	auto n = example.cols() * example.rows();
+	for (uint i = 0; i < n; ++i) {
+		// pick this letter from the embedding
+		input_vector  = G.row_pluck(embedding, example(i));
+		memory        = gate.activate(G, input_vector, initial_state.second[0]);
+		input_vector  = G.eltmul_broadcast_rowwise(input_vector, memory);
+		// pass this letter to the LSTM for processing
+		initial_state = forward_LSTMs(G, input_vector, initial_state, cells);
+		// decoder takes as input the final hidden layer's activation:
+	}
+	vector<utils::OntologyBranch::shared_branch> outputs;
+	// Take the argmax over the available options (0 for go back to
+	// root, and 1..n for the different children of the current position)
+	auto last_turn = argmax_slice(decoder.activate(G, initial_state.second[stack_size-1]), 0, pos->children.size() + 1);
+	// if the turn is 0 go back to root, else go to one of the children using
+	// the lattice pointers:
+	pos = (last_turn == 0) ? root : pos->children[last_turn-1];
+	// add this decision to the output :
+	outputs.emplace_back(pos);
+	for (uint j = 0; j < eval_steps - 1; j++) {
+		input_vector  = G.row_pluck(embedding, pos->id);
+		memory        = gate.activate(G, input_vector, initial_state.second[0]);
+		input_vector  = G.eltmul_broadcast_rowwise(input_vector, memory);
+		initial_state = forward_LSTMs(G, input_vector, initial_state, cells);
+		last_turn     = argmax_slice(decoder.activate(G, initial_state.second[stack_size-1]), 0, pos->children.size() + 1);
+		pos           = (last_turn == 0) ? root : pos->children[last_turn-1];
+		outputs.emplace_back(pos);
+	}
+	return outputs;
+}
+
+// Nested Templates !!
+template<typename T>
+template<typename K>
+string StackedGatedModel<T>::reconstruct_string(
+    K example,
+    const utils::Vocab& lookup_table,
+    int eval_steps,
+    int symbol_offset) {
+	auto reconstruction = reconstruct(example, eval_steps, symbol_offset);
+	stringstream rec;
+	for (auto& cat : reconstruction) {
+		rec << (
+			(cat < lookup_table.index2word.size()) ?
+				lookup_table.index2word.at(cat) :
+				(
+					cat == lookup_table.index2word.size() ? "**END**" : "??"
+				)
+			) << ", ";
+	}
+	return rec.str();
+}
+
+// Nested Templates !!
+template<typename T>
+template<typename K>
+string StackedGatedModel<T>::reconstruct_lattice_string(
+    K example,
+    utils::OntologyBranch::shared_branch root,
+    int eval_steps) {
+	auto reconstruction = reconstruct_lattice(example, root, eval_steps);
+	stringstream rec;
+	for (auto& cat : reconstruction)
+		rec << ((&(*cat) == &(*root)) ? "⟲" : cat->name) << ", ";
+	return rec.str();
+}
+
 typedef Eigen::Block< Eigen::Matrix<uint, Eigen::Dynamic, Eigen::Dynamic>, 1, Eigen::Dynamic, !Eigen::RowMajor> index_row;
 typedef Eigen::VectorBlock< index_row, Eigen::Dynamic> sliced_row;
 
 typedef Eigen::Block< Eigen::Matrix<uint, Eigen::Dynamic, Eigen::Dynamic>, Eigen::Dynamic, 1, !Eigen::RowMajor> index_col;
 typedef Eigen::VectorBlock< index_col, Eigen::Dynamic> sliced_col;
 
-template std::vector<int> StackedGatedModel<float>::reconstruct_fun(sliced_row, utils::Vocab&, int, int);
-template std::vector<int> StackedGatedModel<double>::reconstruct_fun(sliced_row, utils::Vocab&, int, int);
+template string StackedGatedModel<float>::reconstruct_string(sliced_row, const utils::Vocab&, int, int);
+template string StackedGatedModel<double>::reconstruct_string(sliced_row, const utils::Vocab&, int, int);
 
-template std::vector<int> StackedGatedModel<float>::reconstruct_fun(index_row, utils::Vocab&, int, int);
-template std::vector<int> StackedGatedModel<double>::reconstruct_fun(index_row, utils::Vocab&, int, int);
+template string StackedGatedModel<float>::reconstruct_string(index_row, const utils::Vocab&, int, int);
+template string StackedGatedModel<double>::reconstruct_string(index_row, const utils::Vocab&, int, int);
 
-template std::vector<int> StackedGatedModel<float>::reconstruct_fun(sliced_col, utils::Vocab&, int, int);
-template std::vector<int> StackedGatedModel<double>::reconstruct_fun(sliced_col, utils::Vocab&, int, int);
+template string StackedGatedModel<float>::reconstruct_string(sliced_col, const utils::Vocab&, int, int);
+template string StackedGatedModel<double>::reconstruct_string(sliced_col, const utils::Vocab&, int, int);
 
-template std::vector<int> StackedGatedModel<float>::reconstruct_fun(index_col, utils::Vocab&, int, int);
-template std::vector<int> StackedGatedModel<double>::reconstruct_fun(index_col, utils::Vocab&, int, int);
+template string StackedGatedModel<float>::reconstruct_string(index_col, const utils::Vocab&, int, int);
+template string StackedGatedModel<double>::reconstruct_string(index_col, const utils::Vocab&, int, int);
+
+template vector<int> StackedGatedModel<float>::reconstruct(sliced_row, int, int);
+template vector<int> StackedGatedModel<double>::reconstruct(sliced_row, int, int);
+
+template vector<int> StackedGatedModel<float>::reconstruct(index_row, int, int);
+template vector<int> StackedGatedModel<double>::reconstruct(index_row, int, int);
+
+template vector<int> StackedGatedModel<float>::reconstruct(sliced_col, int, int);
+template vector<int> StackedGatedModel<double>::reconstruct(sliced_col, int, int);
+
+template vector<int> StackedGatedModel<float>::reconstruct(index_col, int, int);
+template vector<int> StackedGatedModel<double>::reconstruct(index_col, int, int);
+
+template vector<utils::OntologyBranch::shared_branch> StackedGatedModel<float>::reconstruct_lattice(sliced_row, utils::OntologyBranch::shared_branch, int);
+template vector<utils::OntologyBranch::shared_branch> StackedGatedModel<double>::reconstruct_lattice(sliced_row, utils::OntologyBranch::shared_branch, int);
+
+template vector<utils::OntologyBranch::shared_branch> StackedGatedModel<float>::reconstruct_lattice(index_row, utils::OntologyBranch::shared_branch, int);
+template vector<utils::OntologyBranch::shared_branch> StackedGatedModel<double>::reconstruct_lattice(index_row, utils::OntologyBranch::shared_branch, int);
+
+template vector<utils::OntologyBranch::shared_branch> StackedGatedModel<float>::reconstruct_lattice(sliced_col, utils::OntologyBranch::shared_branch, int);
+template vector<utils::OntologyBranch::shared_branch> StackedGatedModel<double>::reconstruct_lattice(sliced_col, utils::OntologyBranch::shared_branch, int);
+
+template vector<utils::OntologyBranch::shared_branch> StackedGatedModel<float>::reconstruct_lattice(index_col, utils::OntologyBranch::shared_branch, int);
+template vector<utils::OntologyBranch::shared_branch> StackedGatedModel<double>::reconstruct_lattice(index_col, utils::OntologyBranch::shared_branch, int);
+
+template string StackedGatedModel<float>::reconstruct_lattice_string(sliced_row, utils::OntologyBranch::shared_branch, int);
+template string StackedGatedModel<double>::reconstruct_lattice_string(sliced_row, utils::OntologyBranch::shared_branch, int);
+
+template string StackedGatedModel<float>::reconstruct_lattice_string(index_row, utils::OntologyBranch::shared_branch, int);
+template string StackedGatedModel<double>::reconstruct_lattice_string(index_row, utils::OntologyBranch::shared_branch, int);
+
+template string StackedGatedModel<float>::reconstruct_lattice_string(sliced_col, utils::OntologyBranch::shared_branch, int);
+template string StackedGatedModel<double>::reconstruct_lattice_string(sliced_col, utils::OntologyBranch::shared_branch, int);
+
+template string StackedGatedModel<float>::reconstruct_lattice_string(index_col, utils::OntologyBranch::shared_branch, int);
+template string StackedGatedModel<double>::reconstruct_lattice_string(index_col, utils::OntologyBranch::shared_branch, int);
 
 template class StackedGatedModel<float>;
 template class StackedGatedModel<double>;
--- cpp/StackedGatedModel.h
+++ cpp/StackedGatedModel.h
@@ -1,10 +1,15 @@
 #ifndef STACKEDGATED_MAT_H
 #define STACKEDGATED_MAT_H
 
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include "utils.h"
 #include "Mat.h"
 #include "Layers.h"
 #include "Softmax.h"
 #include "CrossEntropy.h"
+#include "OptionParser/OptionParser.h"
 /**
 StackedGatedModel
 -----------------
@@ -23,8 +28,6 @@ total memory used (the input gate's total activation).
 **/
 template<typename T>
 class StackedGatedModel {
-	typedef Eigen::Matrix<uint, Eigen::Dynamic, Eigen::Dynamic> index_mat;
-	typedef std::shared_ptr< index_mat > shared_index_mat;
 	typedef LSTM<T>                    lstm;
 	typedef Layer<T>           classifier_t;
 	typedef Mat<T>                      mat;
@@ -40,6 +43,9 @@ class StackedGatedModel {
 	inline void construct_LSTM_cells();
 
 	public:
+		typedef Eigen::Matrix<uint, Eigen::Dynamic, Eigen::Dynamic> index_mat;
+		typedef std::shared_ptr< index_mat > shared_index_mat;
+		
 		int vocabulary_size;
 		const int output_size;
 		const int stack_size;
@@ -53,13 +59,23 @@ class StackedGatedModel {
 		void save_configuration(std::string);
 		void save(std::string);
 		static StackedGatedModel<T> load(std::string);
+		static StackedGatedModel<T> build_from_CLI(optparse::Values&, int, int, bool verbose=true);
 		StackedGatedModel(int, int, int, int, int, T _memory_penalty = 0.3);
 		StackedGatedModel(int, int, int, std::vector<int>&, T _memory_penalty = 0.3);
 		StackedGatedModel(const config_t&);
-		std::tuple<T, T> cost_fun(graph_t&, std::shared_ptr< index_mat >, shared_eigen_index_vector, shared_eigen_index_vector, uint offset=0);
-		
+		static void add_options_to_CLI(optparse::OptionParser&);
+		std::tuple<T, T> masked_predict_cost(graph_t&, shared_index_mat, shared_index_mat, shared_eigen_index_vector, shared_eigen_index_vector, uint offset=0);
 		template<typename K>
-		std::vector<int> reconstruct_fun(K, utils::Vocab&, int, int symbol_offset = 0);
+		std::vector<int> reconstruct(K, int, int symbol_offset = 0);
+
+		template<typename K>
+		std::string reconstruct_string(K, const utils::Vocab&, int, int symbol_offset = 0);
+
+		template<typename K>
+		std::vector<utils::OntologyBranch::shared_branch> reconstruct_lattice(K, utils::OntologyBranch::shared_branch, int);
+
+		template<typename K>
+		std::string reconstruct_lattice_string(K, utils::OntologyBranch::shared_branch, int);
 };
 
 #endif

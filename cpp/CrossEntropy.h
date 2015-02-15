610c4549441617d967639a3a01f223188f0ab89c
Jonathan Raiman <jonathanraiman@gmail.com>
2015-02-15 12:17:12 -0500

    simplified templates for masked entropy and masked sum

--- cpp/CrossEntropy.h
+++ cpp/CrossEntropy.h
@@ -3,20 +3,26 @@
 
 #include "Softmax.h"
 
-template<typename T> T cross_entropy(std::shared_ptr<Mat<T>>, int&);
-template<typename T, typename M> T cross_entropy(std::shared_ptr<Mat<T>>, const M);
+template<typename T>                         T cross_entropy(std::shared_ptr<Mat<T>>, int&);
+template<typename T, typename M>             T cross_entropy(std::shared_ptr<Mat<T>>, const M);
+template<typename T, typename M>             T masked_cross_entropy(std::shared_ptr<Mat<T>>, uint&, int,  int,  const M);
+template<typename T, typename M>             T masked_cross_entropy(std::shared_ptr<Mat<T>>, uint&, uint, uint, const M);
+template<typename T, typename M>             T masked_cross_entropy(std::shared_ptr<Mat<T>>, uint&, int,  uint, const M);
+template<typename T, typename M>             T masked_cross_entropy(std::shared_ptr<Mat<T>>, uint&, uint, int,  const M);
+template<typename T, typename M>             T masked_cross_entropy(std::shared_ptr<Mat<T>>, uint&, shared_eigen_index_vector, shared_eigen_index_vector, const M);
+template<typename T, typename M>             T masked_cross_entropy(std::shared_ptr<Mat<T>>, uint&, shared_eigen_index_vector, int, const M);
+template<typename T, typename M>             T masked_cross_entropy(std::shared_ptr<Mat<T>>, uint&, shared_eigen_index_vector, uint, const M);
+template<typename T, typename M>             T masked_cross_entropy(std::shared_ptr<Mat<T>>, uint&, int, shared_eigen_index_vector, const M);
+template<typename T, typename M>             T masked_cross_entropy(std::shared_ptr<Mat<T>>, uint&, uint, shared_eigen_index_vector, const M);
 
-template<typename T, typename M, typename K, typename F> T masked_cross_entropy(std::shared_ptr<Mat<T>>, uint&, const K&, const F&, const M);
-template<typename T, typename M, typename K> T masked_cross_entropy(std::shared_ptr<Mat<T>>, uint&, const K&, shared_eigen_index_vector, const M);
-template<typename T, typename M, typename F> T masked_cross_entropy(std::shared_ptr<Mat<T>>, uint&, shared_eigen_index_vector, const F&, const M);
-template<typename T, typename M> T masked_cross_entropy(std::shared_ptr<Mat<T>>, uint&, shared_eigen_index_vector, shared_eigen_index_vector, const M);
-
-template<typename T, typename K, typename F> T masked_sum(std::shared_ptr<Mat<T>>, uint&, const K&, const F&, T);
-template<typename T, typename K> T masked_sum(std::shared_ptr<Mat<T>>, uint&, const K&, shared_eigen_index_vector, T);
-template<typename T, typename F> T masked_sum(std::shared_ptr<Mat<T>>, uint&, shared_eigen_index_vector, const F&, T);
-template<typename T> T masked_sum(std::shared_ptr<Mat<T>>, uint&, shared_eigen_index_vector, shared_eigen_index_vector, T);
-
-template<typename T> T masked_sum(std::shared_ptr<Mat<T>>, uint&, int, shared_eigen_index_vector, T);
-template<typename T, typename F> T masked_sum(std::shared_ptr<Mat<T>>, uint&, int, const F&, T);
+template<typename T>                         T masked_sum(std::shared_ptr<Mat<T>>, uint&, uint, uint, const T&);
+template<typename T>                         T masked_sum(std::shared_ptr<Mat<T>>, uint&, int,  int, const T&);
+template<typename T>                         T masked_sum(std::shared_ptr<Mat<T>>, uint&, uint, int, const T&);
+template<typename T>                         T masked_sum(std::shared_ptr<Mat<T>>, uint&, int,  uint, const T&);
+template<typename T>                         T masked_sum(std::shared_ptr<Mat<T>>, uint&, int,  shared_eigen_index_vector, const T&);
+template<typename T>                         T masked_sum(std::shared_ptr<Mat<T>>, uint&, uint, shared_eigen_index_vector, const T&);
+template<typename T>                         T masked_sum(std::shared_ptr<Mat<T>>, uint&, shared_eigen_index_vector, uint, const T&);
+template<typename T>                         T masked_sum(std::shared_ptr<Mat<T>>, uint&, shared_eigen_index_vector, int, const T&);
+template<typename T>                         T masked_sum(std::shared_ptr<Mat<T>>, uint&, shared_eigen_index_vector, shared_eigen_index_vector, const T&);
 
 #endif

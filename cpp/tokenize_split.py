cdbeb40d2610293913de92d4d2c9af2ae37c2fc0
Jonathan Raiman <jonathanraiman@gmail.com>
2015-02-16 01:43:35 -0500

    cute little tokenization utility

---
+++ cpp/tokenize_split.py
@@ -0,0 +1,60 @@
+"""
+Tokenize Split
+--------------
+
+Small Utility for doing text tokenization and splitting into
+training and validation text files with 1 sentence per line.
+
+Uses `xml_cleaner`
+
+"""
+
+from xml_cleaner import to_raw_text_markupless
+import random, sys
+import os
+import io
+
+def clean_and_output(path, outpath, valid_path, valid_percent = 0.1):
+    total_size = os.stat(path).st_size
+    i = 0
+    with open(valid_path, "wt") as fvalid:
+        with open(outpath, "wt") as fout:
+            fin = open(path, "rb")
+            text_fin = io.TextIOWrapper(fin, newline='')
+            for line in text_fin:
+                i+=1
+                if i % 100 == 0:
+                    progress = fin.tell() / total_size
+                    print("█" * (int(20 * progress)) + " %.1f%% \r" % (100 * progress,), end="", flush=True)
+                line = line.split("\t", 1)[1]
+                for sentence in to_raw_text_markupless(line):
+                    if random.random() > valid_percent:
+                        fout.write(" ".join(sentence))
+                        fout.write("\n")
+                    else:
+                        fvalid.write(" ".join(sentence))
+                        fvalid.write("\n")
+            with open(path, "rt") as fin:
+                for line in fin:
+                    line = line.split("\t", 1)[1]
+                    for sentence in to_raw_text_markupless(line):
+                        if random.random() > valid_percent:
+                            fout.write(" ".join(sentence))
+                            fout.write("\n")
+                        else:
+                            fvalid.write(" ".join(sentence))
+                            fvalid.write("\n") 
+
+usage = """
+{0} [input_file] [output_train_file] [output_validation_file] [(optional) validation percentage]
+"""
+
+if __name__ == "__main__":
+    if len(sys.argv) < 4 or len(sys.argv) > 5:
+        print(usage.format(sys.argv[0]))
+    else:
+        path = sys.argv[1]
+        outpath = sys.argv[2]
+        valid_path = sys.argv[3] 
+        valid_percent = float(sys.argv[4]) if len(sys.argv) == 5 else 0.1
+        clean_and_output(path, outpath, valid_path, valid_percent)

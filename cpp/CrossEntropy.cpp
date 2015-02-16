37a5460d39a6398ba2a4dd08aba45abb805dc65f
Jonathan Raiman <jonathanraiman@gmail.com>
2015-02-15 23:56:47 -0500

    reporting only error methods for masked sum and masked entropy

--- cpp/CrossEntropy.cpp
+++ cpp/CrossEntropy.cpp
@@ -241,14 +241,154 @@ Z masked_cross_entropy(std::shared_ptr<Mat<Z>> logprobs,
 	return cost;
 }
 
+// MASKED CROSS ENTROPY NO GRAD
+
+/**
+Masked Cross Entropy Loss
+-------------------------
+
+(does not calculate gradient, only reports error)
+
+See `masked_cross_entropy`
+
+*/
+template<typename Z, typename M>
+Z masked_cross_entropy_no_grad(std::shared_ptr<Mat<Z>> logprobs,
+	uint& T,
+	shared_eigen_index_vector loss_start,
+	shared_eigen_index_vector codelens,
+	const M targets) {
+	std::shared_ptr<Mat<Z>> probs = softmax(logprobs);
+	Z cost = 0.0;
+	// get cost for each pair of target and datastream:
+	for (size_t i = 0; i < targets.rows(); i++)
+		if (T >= (*loss_start)(i) && (T < (*loss_start)(i) + (*codelens)(i) ))
+			cost -= std::log(probs->w(targets(i),i));
+	return cost;
+}
+
+template<typename Z, typename M>
+Z masked_cross_entropy_no_grad(std::shared_ptr<Mat<Z>> logprobs,
+	uint& T,
+	uint loss_start,
+	shared_eigen_index_vector codelens,
+	const M targets) {
+	std::shared_ptr<Mat<Z>> probs = softmax(logprobs);
+	Z cost = 0.0;
+	// get cost for each pair of target and datastream:
+	for (size_t i = 0; i < targets.rows(); i++)
+		if (T >= loss_start && (T < loss_start + (*codelens)(i) ))
+			cost -= std::log(probs->w(targets(i),i));
+	return cost;
+}
+template<typename Z, typename M>
+Z masked_cross_entropy_no_grad(std::shared_ptr<Mat<Z>> logprobs,
+	uint& T,
+	int loss_start,
+	shared_eigen_index_vector codelens,
+	const M targets) {
+	std::shared_ptr<Mat<Z>> probs = softmax(logprobs);
+	Z cost = 0.0;
+	// get cost for each pair of target and datastream:
+	for (size_t i = 0; i < targets.rows(); i++)
+		if (T >= loss_start && (T < loss_start + (*codelens)(i) ))
+			cost -= std::log(probs->w(targets(i),i));
+	return cost;
+}
+
+template<typename Z, typename M>
+Z masked_cross_entropy_no_grad(std::shared_ptr<Mat<Z>> logprobs,
+	uint& T,
+	shared_eigen_index_vector loss_start,
+	int codelens,
+	const M targets) {
+	std::shared_ptr<Mat<Z>> probs = softmax(logprobs);
+	Z cost = 0.0;
+	// get cost for each pair of target and datastream:
+	for (size_t i = 0; i < targets.rows(); i++)
+		if (T >= (*loss_start)(i) && (T < (*loss_start)(i) + codelens))
+			cost -= std::log(probs->w(targets(i),i));
+	return cost;
+}
+template<typename Z, typename M>
+Z masked_cross_entropy_no_grad(std::shared_ptr<Mat<Z>> logprobs,
+	uint& T,
+	shared_eigen_index_vector loss_start,
+	uint codelens,
+	const M targets) {
+	std::shared_ptr<Mat<Z>> probs = softmax(logprobs);
+	Z cost = 0.0;
+	// get cost for each pair of target and datastream:
+	for (size_t i = 0; i < targets.rows(); i++)
+		if (T >= (*loss_start)(i) && (T < (*loss_start)(i) + codelens))
+			cost -= std::log(probs->w(targets(i),i));
+	return cost;
+}
+template<typename Z, typename M>
+Z masked_cross_entropy_no_grad(std::shared_ptr<Mat<Z>> logprobs,
+	uint& T,
+	int loss_start,
+	int codelens,
+	const M targets) {
+	std::shared_ptr<Mat<Z>> probs = softmax(logprobs);
+	Z cost = 0.0;
+	// get cost for each pair of target and datastream:
+	for (size_t i = 0; i < targets.rows(); i++)
+		if (T >= loss_start && (T < loss_start + codelens))
+			cost -= std::log(probs->w(targets(i),i));
+	return cost;
+}
+template<typename Z, typename M>
+Z masked_cross_entropy_no_grad(std::shared_ptr<Mat<Z>> logprobs,
+	uint& T,
+	uint loss_start,
+	uint codelens,
+	const M targets) {
+	std::shared_ptr<Mat<Z>> probs = softmax(logprobs);
+	Z cost = 0.0;
+	// get cost for each pair of target and datastream:
+	for (size_t i = 0; i < targets.rows(); i++)
+		if (T >= loss_start && (T < loss_start + codelens))
+			cost -= std::log(probs->w(targets(i),i));
+	return cost;
+}
+template<typename Z, typename M>
+Z masked_cross_entropy_no_grad(std::shared_ptr<Mat<Z>> logprobs,
+	uint& T,
+	int loss_start,
+	uint codelens,
+	const M targets) {
+	std::shared_ptr<Mat<Z>> probs = softmax(logprobs);
+	Z cost = 0.0;
+	// get cost for each pair of target and datastream:
+	for (size_t i = 0; i < targets.rows(); i++)
+		if (T >= loss_start && (T < loss_start + codelens))
+			cost -= std::log(probs->w(targets(i),i));
+	return cost;
+}
+template<typename Z, typename M>
+Z masked_cross_entropy_no_grad(std::shared_ptr<Mat<Z>> logprobs,
+	uint& T,
+	uint loss_start,
+	int codelens,
+	const M targets) {
+	std::shared_ptr<Mat<Z>> probs = softmax(logprobs);
+	Z cost = 0.0;
+	// get cost for each pair of target and datastream:
+	for (size_t i = 0; i < targets.rows(); i++)
+		if (T >= loss_start && (T < loss_start + codelens))
+			cost -= std::log(probs->w(targets(i),i));
+	return cost;
+}
+
 template float  cross_entropy(std::shared_ptr<Mat<float>>, eigen_index_block);
 template double cross_entropy(std::shared_ptr<Mat<double>>, eigen_index_block);
 template float  cross_entropy(std::shared_ptr<Mat<float>>, eigen_index_block_scalar);
 template double cross_entropy(std::shared_ptr<Mat<double>>, eigen_index_block_scalar);
 
 /**
-Masked Sum
-----------
+Masked Sum No Grad
+------------------
 
 Sum values[k] if timestep T
 if within [loss_start[k], loss_start[k] + codelens[k]),
@@ -417,6 +557,139 @@ Z masked_sum(std::shared_ptr<Mat<Z>> values,
 	return cost;
 }
 
+// MASKED SUM NO GRAD
+
+/**
+Masked Sum No Grad
+------------------
+
+(does not calculate gradient, only reports error)
+
+See `masked_sum`
+
+*/
+template<typename Z>
+Z masked_sum_no_grad(std::shared_ptr<Mat<Z>> values,
+	uint& T,
+	shared_eigen_index_vector loss_start,
+	shared_eigen_index_vector codelens,
+	const Z& gparent) {
+
+	Z cost = 0.0;
+	// get cost for each pair of target and datastream:
+	for (size_t i = 0; i < values->d; i++)
+		if (T >= (*loss_start)(i) && (T < (*loss_start)(i) + (*codelens)(i) ))
+			cost += values->w.col(i).sum() * gparent;
+	return cost;
+}
+
+template<typename Z>
+Z masked_sum_no_grad(std::shared_ptr<Mat<Z>> values,
+	uint& T,
+	shared_eigen_index_vector loss_start,
+	int codelens,
+	const Z& gparent) {
+	Z cost = 0.0;
+	// get cost for each pair of target and datastream:
+	for (size_t i = 0; i < values->d; i++)
+		if (T >= (*loss_start)(i) && (T < (*loss_start)(i) + codelens ))
+			cost += values->w.col(i).sum() * gparent;
+	return cost;
+}
+template<typename Z>
+Z masked_sum_no_grad(std::shared_ptr<Mat<Z>> values,
+	uint& T,
+	shared_eigen_index_vector loss_start,
+	uint codelens,
+	const Z& gparent) {
+	Z cost = 0.0;
+	// get cost for each pair of target and datastream:
+	for (size_t i = 0; i < values->d; i++)
+		if (T >= (*loss_start)(i) && (T < (*loss_start)(i) + codelens ))
+			cost += values->w.col(i).sum() * gparent;
+	return cost;
+}
+
+template<typename Z>
+Z masked_sum_no_grad(std::shared_ptr<Mat<Z>> values,
+	uint& T,
+	uint loss_start,
+	shared_eigen_index_vector codelens,
+	const Z& gparent) {
+	Z cost = 0.0;
+	// get cost for each pair of target and datastream:
+	for (size_t i = 0; i < values->d; i++)
+		if (T >= loss_start && (T < loss_start + (*codelens)(i) ))
+			cost += values->w.col(i).sum() * gparent;
+	return cost;
+}
+template<typename Z>
+Z masked_sum_no_grad(std::shared_ptr<Mat<Z>> values,
+	uint& T,
+	int loss_start,
+	shared_eigen_index_vector codelens,
+	const Z& gparent) {
+	Z cost = 0.0;
+	// get cost for each pair of target and datastream:
+	for (size_t i = 0; i < values->d; i++)
+		if (T >= loss_start && (T < loss_start + (*codelens)(i) ))
+			cost += values->w.col(i).sum() * gparent;
+	return cost;
+}
+
+template<typename Z>
+Z masked_sum_no_grad(std::shared_ptr<Mat<Z>> values,
+	uint& T,
+	uint loss_start,
+	uint codelens,
+	const Z& gparent) {
+	Z cost = 0.0;
+	// get cost for each pair of target and datastream:
+	for (size_t i = 0; i < values->d; i++)
+		if (T >= loss_start && (T < loss_start + codelens ))
+			cost += values->w.col(i).sum() * gparent;
+	return cost;
+}
+template<typename Z>
+Z masked_sum_no_grad(std::shared_ptr<Mat<Z>> values,
+	uint& T,
+	int loss_start,
+	int codelens,
+	const Z& gparent) {
+	Z cost = 0.0;
+	// get cost for each pair of target and datastream:
+	for (size_t i = 0; i < values->d; i++)
+		if (T >= loss_start && (T < loss_start + codelens ))
+			cost += values->w.col(i).sum() * gparent;
+	return cost;
+}
+template<typename Z>
+Z masked_sum_no_grad(std::shared_ptr<Mat<Z>> values,
+	uint& T,
+	uint loss_start,
+	int codelens,
+	const Z& gparent) {
+	Z cost = 0.0;
+	// get cost for each pair of target and datastream:
+	for (size_t i = 0; i < values->d; i++)
+		if (T >= loss_start && (T < loss_start + codelens ))
+			cost += values->w.col(i).sum() * gparent;
+	return cost;
+}
+template<typename Z>
+Z masked_sum_no_grad(std::shared_ptr<Mat<Z>> values,
+	uint& T,
+	int loss_start,
+	uint codelens,
+	const Z& gparent) {
+	Z cost = 0.0;
+	// get cost for each pair of target and datastream:
+	for (size_t i = 0; i < values->d; i++)
+		if (T >= loss_start && (T < loss_start + codelens ))
+			cost += values->w.col(i).sum() * gparent;
+	return cost;
+}
+
 template float cross_entropy(std::shared_ptr<Mat<float>>, int&);
 template double cross_entropy(std::shared_ptr<Mat<double>>, int&);
 
@@ -456,6 +729,42 @@ template double masked_cross_entropy(std::shared_ptr<Mat<double>>, uint&, int, u
 template double masked_cross_entropy(std::shared_ptr<Mat<double>>, uint&, int, uint, const eigen_index_block_scalar);
 template double masked_cross_entropy(std::shared_ptr<Mat<double>>, uint&, int, int, const eigen_index_block);
 template double masked_cross_entropy(std::shared_ptr<Mat<double>>, uint&, int, int, const eigen_index_block_scalar);
+template float masked_cross_entropy_no_grad(std::shared_ptr<Mat<float>>, uint&, shared_eigen_index_vector, shared_eigen_index_vector, const eigen_index_block);
+template float masked_cross_entropy_no_grad(std::shared_ptr<Mat<float>>, uint&, shared_eigen_index_vector, shared_eigen_index_vector, const eigen_index_block_scalar);
+template float masked_cross_entropy_no_grad(std::shared_ptr<Mat<float>>, uint&, shared_eigen_index_vector, uint, const eigen_index_block);
+template float masked_cross_entropy_no_grad(std::shared_ptr<Mat<float>>, uint&, shared_eigen_index_vector, uint, const eigen_index_block_scalar);
+template float masked_cross_entropy_no_grad(std::shared_ptr<Mat<float>>, uint&, shared_eigen_index_vector, int, const eigen_index_block);
+template float masked_cross_entropy_no_grad(std::shared_ptr<Mat<float>>, uint&, shared_eigen_index_vector, int, const eigen_index_block_scalar);
+template float masked_cross_entropy_no_grad(std::shared_ptr<Mat<float>>, uint&, uint, shared_eigen_index_vector, const eigen_index_block);
+template float masked_cross_entropy_no_grad(std::shared_ptr<Mat<float>>, uint&, uint, shared_eigen_index_vector, const eigen_index_block_scalar);
+template float masked_cross_entropy_no_grad(std::shared_ptr<Mat<float>>, uint&, uint, uint, const eigen_index_block);
+template float masked_cross_entropy_no_grad(std::shared_ptr<Mat<float>>, uint&, uint, uint, const eigen_index_block_scalar);
+template float masked_cross_entropy_no_grad(std::shared_ptr<Mat<float>>, uint&, uint, int, const eigen_index_block);
+template float masked_cross_entropy_no_grad(std::shared_ptr<Mat<float>>, uint&, uint, int, const eigen_index_block_scalar);
+template float masked_cross_entropy_no_grad(std::shared_ptr<Mat<float>>, uint&, int, shared_eigen_index_vector, const eigen_index_block);
+template float masked_cross_entropy_no_grad(std::shared_ptr<Mat<float>>, uint&, int, shared_eigen_index_vector, const eigen_index_block_scalar);
+template float masked_cross_entropy_no_grad(std::shared_ptr<Mat<float>>, uint&, int, uint, const eigen_index_block);
+template float masked_cross_entropy_no_grad(std::shared_ptr<Mat<float>>, uint&, int, uint, const eigen_index_block_scalar);
+template float masked_cross_entropy_no_grad(std::shared_ptr<Mat<float>>, uint&, int, int, const eigen_index_block);
+template float masked_cross_entropy_no_grad(std::shared_ptr<Mat<float>>, uint&, int, int, const eigen_index_block_scalar);
+template double masked_cross_entropy_no_grad(std::shared_ptr<Mat<double>>, uint&, shared_eigen_index_vector, shared_eigen_index_vector, const eigen_index_block);
+template double masked_cross_entropy_no_grad(std::shared_ptr<Mat<double>>, uint&, shared_eigen_index_vector, shared_eigen_index_vector, const eigen_index_block_scalar);
+template double masked_cross_entropy_no_grad(std::shared_ptr<Mat<double>>, uint&, shared_eigen_index_vector, uint, const eigen_index_block);
+template double masked_cross_entropy_no_grad(std::shared_ptr<Mat<double>>, uint&, shared_eigen_index_vector, uint, const eigen_index_block_scalar);
+template double masked_cross_entropy_no_grad(std::shared_ptr<Mat<double>>, uint&, shared_eigen_index_vector, int, const eigen_index_block);
+template double masked_cross_entropy_no_grad(std::shared_ptr<Mat<double>>, uint&, shared_eigen_index_vector, int, const eigen_index_block_scalar);
+template double masked_cross_entropy_no_grad(std::shared_ptr<Mat<double>>, uint&, uint, shared_eigen_index_vector, const eigen_index_block);
+template double masked_cross_entropy_no_grad(std::shared_ptr<Mat<double>>, uint&, uint, shared_eigen_index_vector, const eigen_index_block_scalar);
+template double masked_cross_entropy_no_grad(std::shared_ptr<Mat<double>>, uint&, uint, uint, const eigen_index_block);
+template double masked_cross_entropy_no_grad(std::shared_ptr<Mat<double>>, uint&, uint, uint, const eigen_index_block_scalar);
+template double masked_cross_entropy_no_grad(std::shared_ptr<Mat<double>>, uint&, uint, int, const eigen_index_block);
+template double masked_cross_entropy_no_grad(std::shared_ptr<Mat<double>>, uint&, uint, int, const eigen_index_block_scalar);
+template double masked_cross_entropy_no_grad(std::shared_ptr<Mat<double>>, uint&, int, shared_eigen_index_vector, const eigen_index_block);
+template double masked_cross_entropy_no_grad(std::shared_ptr<Mat<double>>, uint&, int, shared_eigen_index_vector, const eigen_index_block_scalar);
+template double masked_cross_entropy_no_grad(std::shared_ptr<Mat<double>>, uint&, int, uint, const eigen_index_block);
+template double masked_cross_entropy_no_grad(std::shared_ptr<Mat<double>>, uint&, int, uint, const eigen_index_block_scalar);
+template double masked_cross_entropy_no_grad(std::shared_ptr<Mat<double>>, uint&, int, int, const eigen_index_block);
+template double masked_cross_entropy_no_grad(std::shared_ptr<Mat<double>>, uint&, int, int, const eigen_index_block_scalar);
 template float masked_sum(std::shared_ptr<Mat<float>>, uint&, shared_eigen_index_vector, shared_eigen_index_vector, const float&);
 template float masked_sum(std::shared_ptr<Mat<float>>, uint&, shared_eigen_index_vector, uint, const float&);
 template float masked_sum(std::shared_ptr<Mat<float>>, uint&, shared_eigen_index_vector, int, const float&);
@@ -474,3 +783,21 @@ template double masked_sum(std::shared_ptr<Mat<double>>, uint&, uint, int, const
 template double masked_sum(std::shared_ptr<Mat<double>>, uint&, int, shared_eigen_index_vector, const double&);
 template double masked_sum(std::shared_ptr<Mat<double>>, uint&, int, uint, const double&);
 template double masked_sum(std::shared_ptr<Mat<double>>, uint&, int, int, const double&);
+template float masked_sum_no_grad(std::shared_ptr<Mat<float>>, uint&, shared_eigen_index_vector, shared_eigen_index_vector, const float&);
+template float masked_sum_no_grad(std::shared_ptr<Mat<float>>, uint&, shared_eigen_index_vector, uint, const float&);
+template float masked_sum_no_grad(std::shared_ptr<Mat<float>>, uint&, shared_eigen_index_vector, int, const float&);
+template float masked_sum_no_grad(std::shared_ptr<Mat<float>>, uint&, uint, shared_eigen_index_vector, const float&);
+template float masked_sum_no_grad(std::shared_ptr<Mat<float>>, uint&, uint, uint, const float&);
+template float masked_sum_no_grad(std::shared_ptr<Mat<float>>, uint&, uint, int, const float&);
+template float masked_sum_no_grad(std::shared_ptr<Mat<float>>, uint&, int, shared_eigen_index_vector, const float&);
+template float masked_sum_no_grad(std::shared_ptr<Mat<float>>, uint&, int, uint, const float&);
+template float masked_sum_no_grad(std::shared_ptr<Mat<float>>, uint&, int, int, const float&);
+template double masked_sum_no_grad(std::shared_ptr<Mat<double>>, uint&, shared_eigen_index_vector, shared_eigen_index_vector, const double&);
+template double masked_sum_no_grad(std::shared_ptr<Mat<double>>, uint&, shared_eigen_index_vector, uint, const double&);
+template double masked_sum_no_grad(std::shared_ptr<Mat<double>>, uint&, shared_eigen_index_vector, int, const double&);
+template double masked_sum_no_grad(std::shared_ptr<Mat<double>>, uint&, uint, shared_eigen_index_vector, const double&);
+template double masked_sum_no_grad(std::shared_ptr<Mat<double>>, uint&, uint, uint, const double&);
+template double masked_sum_no_grad(std::shared_ptr<Mat<double>>, uint&, uint, int, const double&);
+template double masked_sum_no_grad(std::shared_ptr<Mat<double>>, uint&, int, shared_eigen_index_vector, const double&);
+template double masked_sum_no_grad(std::shared_ptr<Mat<double>>, uint&, int, uint, const double&);
+template double masked_sum_no_grad(std::shared_ptr<Mat<double>>, uint&, int, int, const double&);
--- cpp/CrossEntropy.h
+++ cpp/CrossEntropy.h
@@ -5,6 +5,7 @@
 
 template<typename T>                         T cross_entropy(std::shared_ptr<Mat<T>>, int&);
 template<typename T, typename M>             T cross_entropy(std::shared_ptr<Mat<T>>, const M);
+
 template<typename T, typename M>             T masked_cross_entropy(std::shared_ptr<Mat<T>>, uint&, int,  int,  const M);
 template<typename T, typename M>             T masked_cross_entropy(std::shared_ptr<Mat<T>>, uint&, uint, uint, const M);
 template<typename T, typename M>             T masked_cross_entropy(std::shared_ptr<Mat<T>>, uint&, int,  uint, const M);
@@ -15,6 +16,16 @@ template<typename T, typename M>             T masked_cross_entropy(std::shared_
 template<typename T, typename M>             T masked_cross_entropy(std::shared_ptr<Mat<T>>, uint&, int, shared_eigen_index_vector, const M);
 template<typename T, typename M>             T masked_cross_entropy(std::shared_ptr<Mat<T>>, uint&, uint, shared_eigen_index_vector, const M);
 
+template<typename T, typename M>             T masked_cross_entropy_no_grad(std::shared_ptr<Mat<T>>, uint&, int,  int,  const M);
+template<typename T, typename M>             T masked_cross_entropy_no_grad(std::shared_ptr<Mat<T>>, uint&, uint, uint, const M);
+template<typename T, typename M>             T masked_cross_entropy_no_grad(std::shared_ptr<Mat<T>>, uint&, int,  uint, const M);
+template<typename T, typename M>             T masked_cross_entropy_no_grad(std::shared_ptr<Mat<T>>, uint&, uint, int,  const M);
+template<typename T, typename M>             T masked_cross_entropy_no_grad(std::shared_ptr<Mat<T>>, uint&, shared_eigen_index_vector, shared_eigen_index_vector, const M);
+template<typename T, typename M>             T masked_cross_entropy_no_grad(std::shared_ptr<Mat<T>>, uint&, shared_eigen_index_vector, int, const M);
+template<typename T, typename M>             T masked_cross_entropy_no_grad(std::shared_ptr<Mat<T>>, uint&, shared_eigen_index_vector, uint, const M);
+template<typename T, typename M>             T masked_cross_entropy_no_grad(std::shared_ptr<Mat<T>>, uint&, int, shared_eigen_index_vector, const M);
+template<typename T, typename M>             T masked_cross_entropy_no_grad(std::shared_ptr<Mat<T>>, uint&, uint, shared_eigen_index_vector, const M);
+
 template<typename T>                         T masked_sum(std::shared_ptr<Mat<T>>, uint&, uint, uint, const T&);
 template<typename T>                         T masked_sum(std::shared_ptr<Mat<T>>, uint&, int,  int, const T&);
 template<typename T>                         T masked_sum(std::shared_ptr<Mat<T>>, uint&, uint, int, const T&);
@@ -24,5 +35,15 @@ template<typename T>                         T masked_sum(std::shared_ptr<Mat<T>
 template<typename T>                         T masked_sum(std::shared_ptr<Mat<T>>, uint&, shared_eigen_index_vector, uint, const T&);
 template<typename T>                         T masked_sum(std::shared_ptr<Mat<T>>, uint&, shared_eigen_index_vector, int, const T&);
 template<typename T>                         T masked_sum(std::shared_ptr<Mat<T>>, uint&, shared_eigen_index_vector, shared_eigen_index_vector, const T&);
+
+template<typename T>                         T masked_sum_no_grad(std::shared_ptr<Mat<T>>, uint&, uint, uint, const T&);
+template<typename T>                         T masked_sum_no_grad(std::shared_ptr<Mat<T>>, uint&, int,  int, const T&);
+template<typename T>                         T masked_sum_no_grad(std::shared_ptr<Mat<T>>, uint&, uint, int, const T&);
+template<typename T>                         T masked_sum_no_grad(std::shared_ptr<Mat<T>>, uint&, int,  uint, const T&);
+template<typename T>                         T masked_sum_no_grad(std::shared_ptr<Mat<T>>, uint&, int,  shared_eigen_index_vector, const T&);
+template<typename T>                         T masked_sum_no_grad(std::shared_ptr<Mat<T>>, uint&, uint, shared_eigen_index_vector, const T&);
+template<typename T>                         T masked_sum_no_grad(std::shared_ptr<Mat<T>>, uint&, shared_eigen_index_vector, uint, const T&);
+template<typename T>                         T masked_sum_no_grad(std::shared_ptr<Mat<T>>, uint&, shared_eigen_index_vector, int, const T&);
+template<typename T>                         T masked_sum_no_grad(std::shared_ptr<Mat<T>>, uint&, shared_eigen_index_vector, shared_eigen_index_vector, const T&);
 
 #endif

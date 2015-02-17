4e04bf151005e8f25a3fb6fc4ac8b11667c7884d
Jonathan Raiman <jonathanraiman@gmail.com>
2015-02-17 12:21:56 -0500

    added colors to fix dylib and made makefile less buggy with lblas

--- cpp/fix_dylib.sh
+++ cpp/fix_dylib.sh
@@ -9,6 +9,9 @@ set -o pipefail
 
 INTEL_THREAD_FILE=/opt/intel/mkl/lib/libmkl_intel_thread.dylib
 IOMP5_FILE=/opt/intel/composer_xe_2015.1.108/compiler/lib/libiomp5.dylib
+red=`tput setaf 1`
+green=`tput setaf 2`
+reset=`tput sgr0`
 
 for file in *.o */*.o
 do
@@ -27,9 +30,18 @@ done
 if [ -w "$INTEL_THREAD_FILE" ]
 then
 	install_name_tool -change libiomp5.dylib $IOMP5_FILE $INTEL_THREAD_FILE
-    echo "Achievement Unlocked !!"
-    echo ""
-    echo "Linking fixed on all files"
+	echo "${green}"
+	echo "    ┌───────────────────────┐"
+    echo "    │Achievement Unlocked !!│"
+    echo "    └───────────────────────┘"
+    echo "${reset}"
+    echo " -> Linking fixed on all files"
 else
-	echo "Could not fix linking for $INTEL_THREAD_FILE rerun with 'sudo'"
+	echo "${red}"
+	echo "    ┌────────┐"
+    echo "    │FAILURE!│"
+    echo "    └────────┘"
+    echo "${reset}"
+	echo " -> Could not fix linking for $INTEL_THREAD_FILE"
+	echo "    rerun with 'sudo'"
 fi

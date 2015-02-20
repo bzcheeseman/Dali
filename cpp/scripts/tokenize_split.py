d62200611669a541c99d31ebae64e9bf11f89d9e
Szymon Sidor <szymon.sidor@gmail.com>
2015-02-19 19:52:06 -0500

    Added flags to tokenize split file

--- cpp/scripts/tokenize_split.py
+++ cpp/scripts/tokenize_split.py
@@ -8,45 +8,42 @@ training and validation text files with 1 sentence per line.
 Uses `xml_cleaner`
 
 """
+import argparse
+import io
+import os
+import random, sys
 
 from xml_cleaner import to_raw_text_markupless
-import random, sys
-import os
-import io
 
-def clean_and_output(path, outpath, valid_path, valid_percent = 0.1, perform_split = False):
-    total_size = os.stat(path).st_size
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-i", "--input", help="Input file")
+parser.add_argument("-t", "--train", help="Output training set")
+parser.add_argument("-v", "--validation", help="Output validation set", default="")
+parser.add_argument("-r", "--split_ratio", help="Train/Validation split", default=0.1, type=float)
+parser.add_argument("-s", "--perform_split", help="should split be performed ?", default=True, type=bool)
+
+args = parser.parse_args()
+
+
+if __name__ == "__main__":
+    total_size = os.stat(args.input).st_size
     i = 0
-    with open(valid_path, "wt") as fvalid:
-        with open(outpath, "wt") as fout:
-            fin = open(path, "rb")
+    with open(args.validation, "wt") as fvalid:
+        with open(args.train, "wt") as fout:
+            fin = open(args.input, "rb")
             text_fin = io.TextIOWrapper(fin, newline='')
             for line in text_fin:
                 i+=1
                 if i % 100 == 0:
                     progress = fin.tell() / total_size
                     print("█" * (int(20 * progress)) + " %.1f%% \r" % (100 * progress,), end="", flush=True)
-                if perform_split: line = line.split("\t", 1)[1]
+                if args.perform_split: line = line.split("\t", 1)[1]
                 for sentence in to_raw_text_markupless(line):
-                    if random.random() > valid_percent:
+                    if random.random() > args.split_ratio:
                         fout.write(" ".join(sentence))
                         fout.write("\n")
                     else:
                         fvalid.write(" ".join(sentence))
                         fvalid.write("\n")
             fin.close()
-
-usage = """
-{0} [input_file] [output_train_file] [output_validation_file] [(optional) validation percentage] [(optional) keep only elements after tab char]
-"""
-
-if __name__ == "__main__":
-    if len(sys.argv) < 4 or len(sys.argv) > 6:
-        print(usage.format(sys.argv[0]))
-    else:
-        path = sys.argv[1]
-        outpath = sys.argv[2]
-        valid_path = sys.argv[3]
-        perform_split = sys.argv[5] == "1" if len(sys.argv) > 5  else False
-        valid_percent = float(sys.argv[4]) if len(sys.argv) >= 4 else 0.1
-        clean_and_output(path, outpath, valid_path, valid_percent, perform_split)

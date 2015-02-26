343e877a750af09fdc9c36a06a861ed22003dfd4
Jonathan Raiman <jonathanraiman@gmail.com>
2015-02-25 23:36:36 -0500

    nlp utils has header guard and includes only in .h file

--- cpp/core/NlpUtils.cpp
+++ cpp/core/NlpUtils.cpp
@@ -1,6 +1,7 @@
-#include "NlpUtils.h"
+#ifndef NLP_UTILS_H
+#define NLP_UTILS_H
 
-#include "utils.h"
+#include "NlpUtils.h"
 
 DEFINE_int32(subsets,            10,
         "Break up dataset into how many minibatches ? \n"
@@ -21,3 +22,5 @@ static bool dummy = GFLAGS_NAMESPACE::RegisterFlagValidator(&FLAGS_train,
                                                             &utils::validate_flag_nonempty);
 static bool dummy1 = GFLAGS_NAMESPACE::RegisterFlagValidator(&FLAGS_validation,
                                                             &utils::validate_flag_nonempty);
+
+#endif

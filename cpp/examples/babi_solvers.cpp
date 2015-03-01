4e797c0ea9a06632d3df4ed7fd1d6b61f65a648c
Szymon Sidor <szymon.sidor@gmail.com>
2015-02-28 19:04:37 -0500

    Dumb solver for babi

--- cpp/examples/babi_solvers.cpp
+++ cpp/examples/babi_solvers.cpp
@@ -1,26 +1,39 @@
 #include <iostream>
+#include <set>
 #include <vector>
 
 #include "core/babi.h"
 
 using std::vector;
 using std::string;
+using std::set;
 
 class DumbModel: public babi::Model {
+    set<string> tokens;
     public:
-
         void train(vector<babi::Story> data) {
-
         }
 
         void new_story() {
-
+            tokens.clear();
         }
         void fact(const vector<string>& fact) {
-
+            for(const string& token: fact) {
+                if (token.compare(".") == 0 || token.compare("?") == 0)
+                    continue;
+                tokens.insert(token);
+            }
         }
         vector<string> question(const vector<string> quesiton) {
-            return {};
+            string ans;
+            int ans_idx = rand()%tokens.size();
+            int current_idx = 0;
+            for(auto& el: tokens) {
+                if (current_idx == ans_idx)
+                    ans = el;
+                ++current_idx;
+            }
+            return {ans};
         }
 };
 

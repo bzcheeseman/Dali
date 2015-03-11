fac7e51d675ad1e7404b483aa27283002d5fd514
Jonathan Raiman <jonathanraiman@gmail.com>
2015-03-11 04:03:28 -0400

    fast builder active

---
+++ cpp/scripts/build_fast.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# stop script on error and print it
+set -e
+# inform me of undefined variables
+set -u
+# handle cascading failures well
+set -o pipefail
+
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+PROJECT_DIR=$( dirname $SCRIPT_DIR )
+
+pushd $PROJECT_DIR/build
+    rm -rf ./*
+    echo "Preparing the Ferrari,"
+    cmake -DCMAKE_BUILD_TYPE=release ..
+    echo "This builds the Ferrari,"
+    make -j 9
+    echo "Now you drive the Ferrari."
+popd

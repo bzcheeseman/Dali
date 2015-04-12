2e8ed16c726376e6af40fad3dddd0c87f789bb4b
Jonathan Raiman <jonathanraiman@gmail.com>
2015-04-12 14:13:16 -0400

    single threaded training, parallel hyperparameters

--- scripts/sentiment/train_grid.sh
+++ scripts/sentiment/train_grid.sh
@@ -47,13 +47,13 @@ function ensure_dir {
 SAVE_FOLDER="$(ensure_dir $1)saved_models"
 RESULTS_FILE="$(ensure_dir $1)results.txt"
 BASE_FLAGS="--results_file=${RESULTS_FILE}"
-BASE_FLAGS="${BASE_FLAGS} --validation_metric=1 --save_location=${SAVE_FOLDER}/model"
+BASE_FLAGS="${BASE_FLAGS} --validation_metric=1"
 BASE_FLAGS="${BASE_FLAGS} --stack_size=${STACK_SIZE} --patience=${PATIENCE}"
-BASE_FLAGS="${BASE_FLAGS} --epochs=2000 -j=${CPU_CORES}"
+BASE_FLAGS="${BASE_FLAGS} --epochs=2000 -j=1"
 BASE_FLAGS="${BASE_FLAGS} --fast_dropout --dropout=0.32 --noshortcut"
 # use some pretrained vectors:
-BASE_FLAGS="${BASE_FLAGS} --pretrained_vectors=${VECTOR_FILE}"
-BASE_FLAGS="${BASE_FLAGS} --embedding_learning_rate 0.1"
+# BASE_FLAGS="${BASE_FLAGS} --pretrained_vectors=${VECTOR_FILE}"
+BASE_FLAGS="${BASE_FLAGS} --embedding_learning_rate -1."
 BASE_FLAGS="${BASE_FLAGS} --train=${DATA_DIR}train.txt "
 BASE_FLAGS="${BASE_FLAGS} --validation=${DATA_DIR}dev.txt "
 BASE_FLAGS="${BASE_FLAGS} --test=${DATA_DIR}test.txt "
@@ -76,11 +76,14 @@ for hidden in 150
 do
     for reg in 0.0004 0.001 0.004
     do
-        for lr in 0.01 0.05 0.1 0.3
+        for lr in 0.001 0.004 0.01 0.05 0.1 0.3
         do
             # previously saved models are no longer useful for this grid tile
-            rm -rf $SAVE_FOLDER/*
-            $PROGRAM $BASE_FLAGS --learning_rate $lr --hidden $hidden --minibatch 25 --solver adagrad --reg $reg
+            if [ ! -d "${SAVE_FOLDER}/model${lr}_${reg}/" ]; then
+                mkdir "${SAVE_FOLDER}/model${lr}_${reg}/"
+            fi
+            rm -rf "${SAVE_FOLDER}/model${lr}_${reg}/*"
+            $PROGRAM $BASE_FLAGS --save_location="${SAVE_FOLDER}/model${lr}_${reg}" --learning_rate $lr --hidden $hidden --minibatch 25 --solver adagrad --reg $reg | tee "${SAVE_FOLDER}/model${lr}_${reg}/progress.txt" &
         done
     done
 done

5d101d75c21333b2f8f15b1412fdb213b61b5e8c
Szymon Sidor <szymon.sidor@gmail.com>
2015-04-12 01:32:57 -0400

    Event Queue now works!

--- examples/visualizer.cpp
+++ examples/visualizer.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <redox.hpp>
+#include <chrono>
+#include <thread>
 
-using namespace std;
-using namespace redox;
+#include "dali/visualizer/EventQueue.h"
 
+using redox::Redox;
+using namespace std::chrono;
 /**
 Simple hello world with Redis Server and Hi-Redis.
 
@@ -18,13 +21,39 @@ redis-server
 **/
 
 int main(int argc, char* argv[]) {
+    redox::Redox rdx;
+    if(!rdx.connect("localhost", 6379)) return 1;
 
-  Redox rdx;
-  if(!rdx.connect("localhost", 6379)) return 1;
+    rdx.set("hello", "world!");
+    std::cout << "Hello, " << rdx.get("hello") << std::endl;
 
-  rdx.set("hello", "world!");
-  cout << "Hello, " << rdx.get("hello") << endl;
+    rdx.disconnect();
 
-  rdx.disconnect();
-  return 0;
+    EventQueue eq;
+    std::cout << "experiment 1" << std::endl;
+
+    {
+        auto handle = eq.run_every([]() {
+            std::cout << "siema" << std::endl;
+        }, milliseconds(1000));
+
+        std::this_thread::sleep_for(seconds(3));
+    }
+    std::this_thread::sleep_for(seconds(3));
+    std::cout << "experiment 1: done!" << std::endl;
+    std::cout << "experiment 2" << std::endl;
+
+
+    auto handle2 = eq.run_every([]() {
+        std::cout << "printed every 5s" << std::endl;
+    }, milliseconds(5000));
+
+    std::this_thread::sleep_for(seconds(6));
+    eq.push([] {
+        std::cout << "printed 1s after previous guy" << std::endl;
+    });
+
+    std::this_thread::sleep_for(seconds(5));
+    std::cout << "experiment 2: done!" << std::endl;
+    return 0;
 }

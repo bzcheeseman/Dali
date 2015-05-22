51a781ab33cb567bcbf5f80e6760b246257e4565
Jonathan Raiman <jonathanraiman@gmail.com>
2015-05-22 00:33:21 -0400

    added futile test to beam

---
+++ dali/execution/tests.cpp
@@ -0,0 +1,20 @@
+#include "Mat.h"
+#include "dali/execution/BeamSearch.h"
+
+TEST(beam_search, beam_search2) {
+    // int target = 8;
+    // auto functor = [&target](vector<Mat<R>> Xs)-> Mat<R> {
+    //     auto soft = MatOps<R>::softmax(
+    //             Xs[1].dot(Xs[0])
+    //         );
+    //     return MatOps<R>::cross_entropy(
+    //         soft,
+    //         target);
+    // };
+    // EXPERIMENT_REPEAT {
+    //     auto input = Mat<R>(5,  3, weights<R>::uniform(-2.0, 2.0));
+    //     auto layer = Mat<R>(10, 5, weights<R>::uniform(-2.0, 2.0));
+    //     ASSERT_TRUE(gradient_same<R>(functor, {input, layer}, 1e-4));
+    // }
+    ASSERT_EQ(1 + 1, 2);
+}

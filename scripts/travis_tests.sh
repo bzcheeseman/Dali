d6c1ac5c3e6d9968cb081d0f90de508a9498d22a
Szymon Sidor <szymon.sidor@gmail.com>
2015-06-12 01:45:41 -0700

    [travis] fixes

--- scripts/travis_tests.sh
+++ scripts/travis_tests.sh
@@ -11,8 +11,9 @@ set -o pipefail
 SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 pushd $SCRIPT_DIR/../build
-    cmake ..
-    make -j9 && make -j9 run_tests
+    cmake -DWITH_CUDA=FALSE ..
+    make -j9 run_tests
+    make -j9
     result=$?
     rm -r ./*
     echo "Unit tests completed : $result"

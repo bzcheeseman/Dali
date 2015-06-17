511165a6c72bd20c1186828fcc9cfc90328b3a1e
Jonathan Raiman <jonathanraiman@gmail.com>
2015-06-17 02:24:44 -0700

    infinite template recursion now supported for transpose + more generic templates

--- examples/templator.cu
+++ examples/templator.cu
@@ -8,6 +8,22 @@ typedef Tensor<cpu, 2, float> cpu_t;
 typedef Tensor<gpu, 2, float> gpu_t;
 
 template<typename LeftType, typename RightType, typename DType, int ktype>
+class Wrapper;
+
+template<typename LeftType, typename RightType, typename DType, int ktype>
+class WrapperTransposed {
+    public:
+        TransposeExp<LeftType, DType> left;
+        TransposeExp<RightType, DType> right;
+
+        WrapperTransposed(
+            const TransposeExp<LeftType, DType>& _left,
+            const TransposeExp<RightType, DType>& _right) : left(_left), right(_right) {}
+
+        inline Wrapper<LeftType, RightType, DType, ktype> T(void) const;
+};
+
+template<typename LeftType, typename RightType, typename DType, int ktype>
 class Wrapper {
     public:
         LeftType left;
@@ -17,18 +33,25 @@ class Wrapper {
             const LeftType& _left,
             const RightType& _right) : left(_left), right(_right) {}
 
-        inline Wrapper<TransposeExp<LeftType, DType>, TransposeExp<RightType, DType>, DType, ktype> T(void) const {
+        inline WrapperTransposed<LeftType, RightType, DType, ktype> T(void) const {
             auto cpu_T = left.T();
             auto gpu_T = right.T();
-            return Wrapper<decltype(cpu_T),decltype(gpu_T), DType, ktype>(cpu_T, gpu_T);
+            return WrapperTransposed<LeftType, RightType, DType, ktype>(cpu_T, gpu_T);
         }
 };
 
+template<typename LeftType, typename RightType, typename DType, int ktype>
+inline Wrapper<LeftType, RightType, DType, ktype> WrapperTransposed<LeftType, RightType, DType, ktype>::T(void) const {
+    auto cpu_T = left.T();
+    auto gpu_T = right.T();
+    return Wrapper<decltype(cpu_T),decltype(gpu_T), DType, ktype>(cpu_T, gpu_T);
+}
+
 #define BINARY_OP(opname, opsymbol) \
-template<typename TA, typename TB, typename TC, typename TD, typename DType, int ta, int tb> \
+template<template <typename, typename, typename, int> class wrapper_t1, template <typename, typename, typename, int> class wrapper_t2, typename TA, typename TB, typename TC, typename TD, typename DType, int ta, int tb> \
 Wrapper< BinaryMapExp<opname, TA, TC, DType, (ta|tb|type::kMapper)>, BinaryMapExp<opname, TB, TD, DType, (ta|tb|type::kMapper)>, DType, (ta|tb|type::kMapper)> operator opsymbol( \
-        const Wrapper<TA, TB, DType, ta> &left, \
-        const Wrapper<TC, TD, DType, tb> &right) { \
+        const wrapper_t1<TA, TB, DType, ta> &left, \
+        const wrapper_t2<TC, TD, DType, tb> &right) { \
     const auto& l_cpu = left.left; \
     const auto& r_cpu = right.left; \
     auto res_cpu = l_cpu opsymbol r_cpu; \
@@ -43,17 +66,17 @@ BINARY_OP(op::mul,   *);
 BINARY_OP(op::minus, -);
 
 /*! \brief make expression */
-template<typename OP, typename TA, typename TB, typename DType, int ta>
-inline Wrapper<UnaryMapExp<OP, TA, DType, (ta|type::kMapper)>, UnaryMapExp<OP, TB, DType, (ta|type::kMapper)>, DType, (ta|type::kMapper)>
-MakeExp(const Wrapper<TA, TB, DType, ta> &src) {
+template<typename OP, template <typename, typename, typename, int> class wrapper_t, typename TA, typename TB, typename DType, int ta>
+inline wrapper_t<UnaryMapExp<OP, TA, DType, (ta|type::kMapper)>, UnaryMapExp<OP, TB, DType, (ta|type::kMapper)>, DType, (ta|type::kMapper)>
+MakeExp(const wrapper_t<TA, TB, DType, ta> &src) {
     auto unary_l = UnaryMapExp<OP, TA, DType, (ta|type::kMapper)>(src.left);
     auto unary_r = UnaryMapExp<OP, TB, DType, (ta|type::kMapper)>(src.right);
-    return Wrapper<decltype(unary_l), decltype(unary_r), DType, (ta|type::kMapper)>(unary_l, unary_r);;
+    return wrapper_t<decltype(unary_l), decltype(unary_r), DType, (ta|type::kMapper)>(unary_l, unary_r);;
 }
 
-template<typename OP, typename TA, typename TB, typename DType, int ta>
-inline Wrapper<UnaryMapExp<OP, TA, DType, (ta|type::kMapper)>, UnaryMapExp<OP, TB, DType, (ta|type::kMapper)>, DType, (ta|type::kMapper)>
-F(const Wrapper<TA, TB, DType, ta> &src) {
+template<typename OP, template <typename, typename, typename, int> class wrapper_t, typename TA, typename TB, typename DType, int ta>
+inline wrapper_t<UnaryMapExp<OP, TA, DType, (ta|type::kMapper)>, UnaryMapExp<OP, TB, DType, (ta|type::kMapper)>, DType, (ta|type::kMapper)>
+F(const wrapper_t<TA, TB, DType, ta> &src) {
     return MakeExp<OP>(src);
 }
 
@@ -81,6 +104,8 @@ int main() {
     auto d = c + b;
 
     auto g = a.T();
+
+    auto gg = g.T().T();
 
     auto f = c * b;
 

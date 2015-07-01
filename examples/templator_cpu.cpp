eae184716b5415388de831201260eecae35d4fe3
Jonathan Raiman <jonathanraiman@gmail.com>
2015-06-30 20:34:03 -0700

    cleared templator_cpu

--- examples/templator_cpu.cpp
+++ examples/templator_cpu.cpp
@@ -1,74 +1,9 @@
 #include <iostream>
 #include <vector>
-#include "mshadow/tensor.h"
 #include "dali/tensor/Mat.h"
-#include "dali/tensor/LazyTensor.h"
-
-using namespace mshadow;
-using namespace mshadow::expr;
 using std::vector;
-
-typedef Tensor<cpu, 2, float> cpu_t;
-
-struct sigmoid {
-    MSHADOW_XINLINE static float Map(float a) {
-        return 1.0f / (1.0f + expf(-a));
-    }
-};
 
 typedef float R;
 
-typedef LazyTensor<cpu_t, R, type::kRValue> wrapped_t;
-
 int main() {
-    Mat<R> A(2, 3, weights<R>::gaussian(2.0));
-    Mat<R> B(2, 3);
-
-    ELOG(A.w()->w.cpu_fresh);
-
-    A.print();
-
-    A.w(0) = 1.0;
-    A.w(1) = 2.0;
-
-    A.print();
-
-    ELOG(A.w()->w.cpu_fresh);
-
-    wrapped_t a(A.w()->w);
-    wrapped_t b(B.w()->w);
-
-    auto c = a + b;
-
-    auto d   = c + b;
-    auto g   = a.T();
-    auto gg  = g.T().T();
-    auto f   = c * b;
-    auto sig = F<sigmoid>(a);
-
-    ELOG(A.w()->w.cpu_fresh);
-
-    auto out = Mat<R>::empty_like(A);
-    out.w()->w = c;
-
-    ELOG(A.w()->w.cpu_fresh);
-
-    out.print();
-
-    auto out2 = Mat<R>::empty_like(A);
-
-    out2.w()->w = c.softmax();
-    out2.print();
-    Mat<R> C(3, 1);
-
-    C.w()->w = c[0].broadcast<0>(C.w()->w.shape());
-
-    C.print();
-
-    Mat<R> D(4, 3);
-
-    D.w()->w = c[0].repmat(4);
-
-    D.print();
-
 }

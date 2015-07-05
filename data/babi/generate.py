0873de01f208626efdf665027e52879bd24ac92d
Jonathan Raiman <jonathanraiman@gmail.com>
2015-07-04 17:03:51 -0700

    made tar file relative

--- data/babi/generate.py
+++ data/babi/generate.py
@@ -26,6 +26,6 @@ if __name__ == '__main__':
     delete_paths([TARBALL_LOCAL, OUTPUT_DIR])
     execute_bash('wget -O %s %s' % (TARBALL_LOCAL, TARBALL_URL))
     execute_bash('mkdir %s' % (OUTPUT_DIR,))
-    execute_bash('tar -xz -f %s' % (TARBALL_LOCAL))
+    execute_bash('tar -xz -f %s -C %s' % (TARBALL_LOCAL, THIS_DATA_DIR))
     execute_bash('mv %s %s' % (OUTPUT_DIR, NORMALIZED_OUTPUT_DIR))
     delete_paths([TARBALL_LOCAL,])

7cc0330eb4339c3170f51ad493a6357ee31bb50d
Szymon Sidor <szymon.sidor@gmail.com>
2016-03-20 20:40:39 -0700

    travis now tests GPU builds

---
+++ scripts/ubuntu_trusty_install_cuda.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Install the CUDA toolkit
+# inspiration: https://github.com/tmcdonell/cuda
+
+if [[ "$WITH_CUDA" == "TRUE" ]]
+then
+    echo "Installing CUDA library"
+    #travis_retry
+    wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_${CUDA}_amd64.deb
+    #travis_retry
+    sudo dpkg -i cuda-repo-ubuntu1404_${CUDA}_amd64.deb
+    #travis_retry
+    sudo apt-get update -qq
+    export CUDA_APT=${CUDA%-*}
+    export CUDA_APT=${CUDA_APT/./-}
+    #travis_retry
+    sudo apt-get install -y cuda-drivers cuda-core-${CUDA_APT} cuda-cudart-dev-${CUDA_APT}
+    #travis_retry
+    sudo apt-get clean
+    export CUDA_HOME=/usr/local/cuda-${CUDA%%-*}
+    export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${LD_LIBRARY_PATH}
+    export PATH=${CUDA_HOME}/bin:${PATH}
+    echo "done"
+else
+    echo "Skipping GPU installation - CPU only."
+fi

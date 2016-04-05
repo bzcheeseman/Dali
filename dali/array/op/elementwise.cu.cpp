e20f249cb3c71afd940e7a675064f46e0f4a6e6c
Jonathan Raiman <jonathanraiman@gmail.com>
2016-04-04 21:09:35 -0700

    less requirements needed for sigmoid

--- dali/array/op/elementwise.cu.cpp
+++ dali/array/op/elementwise.cu.cpp
@@ -1,14 +1,8 @@
 #include "dali/array/op/elementwise.h"
 
-#include <iostream>
-
-#include "dali/utils.h"
 #include "dali/array/array.h"
 #include "dali/array/array_function.h"
-#include "dali/array/memory/device.h"
 #include "dali/array/TensorFunctions.h"
-
-using memory::Device;
 
 typedef Elementwise<TensorOps::op::sigmoid> Sigmoid;
 Array sigmoid(const Array& x) { return Sigmoid::eval(x); }

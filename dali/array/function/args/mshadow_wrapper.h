a49b27cdc4b63d613dfde27d8891bab3445eecdd
Szymon Sidor <szymon.sidor@gmail.com>
2016-04-29 16:17:59 -1000

    Succesfully added Mshadow hijacking expression with hope of injecting striding information soon.

--- dali/array/function/args/mshadow_wrapper.h
+++ dali/array/function/args/mshadow_wrapper.h
@@ -4,9 +4,73 @@
 #include "dali/array/function/typed_array.h"
 #include "dali/array/memory/device.h"
 
-// This file would not be needed at all if we defined to_mshadow_expr function on Array.
-//
-// The reason not to do that is to hide all mshadow usage in cpp files whereever possible.
+////////////////////////////////////////////////////////////////////////////////
+//                           MSHADOW_WRAPPER_EXP                              //
+//                                   ---                                      //
+//  This expression is used to inject Dali striding information to mshadow    //
+//  expression processor                                                      //
+////////////////////////////////////////////////////////////////////////////////
+
+namespace mshadow {
+    namespace expr {
+        template<typename SrcExp, typename DType, int srcdim>
+        struct MshadowWrapperExp: public MakeTensorExp<
+                                            MshadowWrapperExp<SrcExp, DType, srcdim>,
+                                            SrcExp, srcdim, DType
+                                         > {
+            const SrcExp src_;
+
+            MshadowWrapperExp(const SrcExp &src, const Array& dali_src) :
+                    src_(src) {
+                this->shape_ = ShapeCheck<srcdim, SrcExp>::Check(src_);
+            }
+        };
+
+        template<typename SrcExp, typename DType, int etype>
+        inline MshadowWrapperExp<SrcExp, DType, ExpInfo<SrcExp>::kDim>
+        MakeMshadowWrapperExp(const Exp<SrcExp, DType, etype> &src, const Array& dali_src) {
+            return MshadowWrapperExp<SrcExp, DType, ExpInfo<SrcExp>::kDim>(src.self(), dali_src);
+        }
+
+        template<typename SrcExp, typename DType, int srcdim>
+        struct ExpInfo<MshadowWrapperExp<SrcExp, DType, srcdim> > {
+            static const int kDimSrc = ExpInfo<SrcExp>::kDim;
+            static const int kDim = kDimSrc >= 0 ? srcdim : -1;
+            static const int kDevMask = ExpInfo<SrcExp>::kDevMask;
+        };
+
+        template<typename SrcExp, typename DType, int srcdim>
+        struct ShapeCheck<srcdim, MshadowWrapperExp<SrcExp, DType, srcdim> > {
+            inline static Shape<srcdim>
+            Check(const MshadowWrapperExp<SrcExp, DType, srcdim> &t) {
+                return t.shape_;
+            }
+        };
+
+        template<typename SrcExp, typename DType, int srcdim>
+        struct Plan<MshadowWrapperExp<SrcExp, DType, srcdim>, DType> {
+          public:
+            explicit Plan(const MshadowWrapperExp<SrcExp, DType, srcdim> &e) :
+                    src_(MakePlan(e.src_)) {
+            }
+
+            MSHADOW_XINLINE DType Eval(index_t i, index_t j) const {
+                return src_.Eval(i, j);
+            }
+
+          private:
+            Plan<SrcExp, DType> src_;
+        };
+    } //namespace expr
+} // namespace mshadow
+////////////////////////////////////////////////////////////////////////////////
+//                             MSHADOW_WRAPPER                                //
+//                                   ---                                      //
+//  This class would not be needed at all if we defined to_mshadow_expr       //
+//  function on Array. The reason not to do that is to hide all mshadow usage //
+//  in cpp files whereever possible.                                          //
+////////////////////////////////////////////////////////////////////////////////
+
 
 template<int devT,typename T, typename ExprT>
 struct MshadowWrapper {
@@ -19,8 +83,8 @@ struct MshadowWrapper {
 template<int devT,typename T>
 struct MshadowWrapper<devT,T,Array> {
     static inline auto wrap(const Array& array, memory::Device device) ->
-            decltype(TypedArray<devT,T>(array, device).d1()) {
-        return TypedArray<devT,T>(array, device).d1();
+            decltype(MakeMshadowWrapperExp(TypedArray<devT,T>(array, device).d1(), array)) {
+        return MakeMshadowWrapperExp(TypedArray<devT,T>(array, device).d1(), array);
     }
 };
 

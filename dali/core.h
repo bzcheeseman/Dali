caf0f7ab55b0dd1700cb4c9b52a37a9b0fb8c34a
Szymon Sidor <szymon.sidor@gmail.com>
2016-06-11 23:16:12 -0700

    add missing includes to dali/core.h

--- dali/core.h
+++ dali/core.h
@@ -1,17 +1,15 @@
 #ifndef DALI_CORE_H
 #define DALI_CORE_H
-// TODO(szymon): make this complete
-// #include "dali/tensor/Index.h"
-// #include "dali/layers/Layers.h"
+
 #include "dali/array/array.h"
 #include "dali/array/op.h"
+#include "dali/layers/lstm.h"
+#include "dali/layers/gru.h"
+#include "dali/layers/layers.h"
 #include "dali/tensor/tensor.h"
 #include "dali/tensor/op.h"
 #include "dali/tensor/tape.h"
-#include "dali/layers/layers.h"
 #include "dali/tensor/solver.h"
-// #include "dali/layers/LSTM.h"
-// #include "dali/layers/GRU.h"
 // #include "dali/execution/SequenceProbability.h"
 // #include "dali/execution/BeamSearch.h"
 

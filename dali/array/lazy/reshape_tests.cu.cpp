63fee7ecf77301b1b2ce6050017c1a5527629ae1
Jonathan Raiman <jonathanraiman@gmail.com>
2016-06-23 20:51:13 -0700

    make im2col work for nchw and nhwc

--- dali/array/lazy/reshape_tests.cu.cpp
+++ dali/array/lazy/reshape_tests.cu.cpp
@@ -169,17 +169,45 @@ TEST(ArrayReshapeTests, take_from_rows_assign_lazy) {
 }
 
 
-TEST(ArrayReshapeTests, im2col_nhwc) {
-    Array test_image({2, 1, 3, 4}, DTYPE_INT32);
-    test_image = initializer::arange();
-    test_image[1] *= 10;
+TEST(ArrayReshapeTests, im2col_without_channels) {
+    Array image_nchw({2, 1, 3, 4}, DTYPE_INT32);
+    Array image_nhwc = image_nchw.transpose({0, 2, 3, 1});
+
+    image_nchw = initializer::arange();
+    image_nchw[1] *= 10;
 
     Array im2coled_image = lazy::im2col_nchw(
-        test_image, 3, 3, 1, 1
+        image_nchw, 3, 3, 1, 1
     );
-    test_image.print();
-    im2coled_image.print();
-    im2coled_image.reshape({im2coled_image.shape()[0], 2, im2coled_image.shape()[1] / 2}).print();
+    Array im2coled_image2 = lazy::im2col_nhwc(
+        image_nhwc, 3, 3, 1, 1
+    );
+    EXPECT_TRUE(Array::equals(im2coled_image2, im2coled_image));
+}
+
+TEST(ArrayReshapeTests, im2col_with_channels) {
+    Array image_nchw({2, 2, 3, 4}, DTYPE_INT32);
+    Array image_nhwc = image_nchw.transpose({0, 2, 3, 1});
+
+    image_nchw = initializer::arange();
+    image_nchw[1] *= 10;
+    (Array)image_nchw[Slice(0, 2)][0] *= -1.0;
+
+    Array im2coled_image1 = lazy::im2col_nchw(
+        image_nchw, 3, 3, 1, 1
+    );
+    Array im2coled_image1_ch1 = im2coled_image1[Slice(0, im2coled_image1.shape()[0] / 2)];
+    Array im2coled_image1_ch2 = im2coled_image1[Slice(im2coled_image1.shape()[0] / 2, im2coled_image1.shape()[0])];
+
+    Array im2coled_image2 = lazy::im2col_nhwc(
+        image_nhwc, 3, 3, 1, 1
+    );
+
+    Array im2coled_image2_ch1 = im2coled_image2[Slice(0, im2coled_image2.shape()[0], 2)];
+    Array im2coled_image2_ch2 = im2coled_image2[Slice(1, im2coled_image2.shape()[0], 2)];
+
+    EXPECT_TRUE(Array::equals(im2coled_image1_ch1, im2coled_image2_ch1));
+    EXPECT_TRUE(Array::equals(im2coled_image1_ch2, im2coled_image2_ch2));
 }
 
 

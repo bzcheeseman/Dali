68cacad65157ae4ee39d09afbe1e057911e24516
Szymon Sidor <szymon.sidor@gmail.com>
2016-07-03 23:34:07 -0700

    provide a CPU implementation of conv_bias backward, yay

--- dali/array/op/spatial/bias.cu.cpp
+++ dali/array/op/spatial/bias.cu.cpp
@@ -2,10 +2,11 @@
 
 #include "dali/config.h"
 
-#include "dali/runtime_config.h"
 #include "dali/array/array.h"
 #include "dali/array/function/function.h"
 #include "dali/array/op/cudnn_utils.h"
+#include "dali/array/op/spatial/utils.h"
+#include "dali/runtime_config.h"
 
 ///////////////////////////////////////////////////////////////////////////////
 //                    Conv2dBwdBiasFunction                                  //
@@ -17,32 +18,80 @@ struct Conv2dBwdBiasFunction : public Function<Conv2dBwdBiasFunction,
                                         std::string> {
     static std::vector<int> deduce_output_bshape(const Array& out_dw,
                                                  const std::string& data_format) {
-        size_t channel_dim = data_format.find('C');
-        ASSERT2(channel_dim != std::string::npos,
-                utils::MS() << "data_format must be NCHW or NHWC (got " << data_format << ").");
-
-        return {out_dw.shape()[channel_dim]};
+        internal::DataFormatDimMapping mapping(data_format);
+        ASSERT2_SHAPE_ND(out_dw.shape(), 4, "gradient for convolution bias");
+        return {out_dw.bshape()[mapping.c_dim]};
     }
 
-    template<OPERATOR_T operator_t, typename T>
-    void typed_eval(TypedArray<memory::DEVICE_T_CPU, T> bias_dw,
-                    TypedArray<memory::DEVICE_T_CPU, T> out_dw,
+
+    template<OPERATOR_T operator_t, typename T, int devT>
+    void typed_eval(TypedArray<devT, T> bias_dw,
+                    TypedArray<devT, T> out_dw,
                     const std::string& data_format) {
-        throw std::runtime_error("not implemented!");
+#ifdef DALI_USE_CUDNN
+        if (use_cudnn && devT == memory::DEVICE_T_GPU &&
+                !std::is_same<T, int>::value &&
+                operator_t != OPERATOR_T_MUL &&
+                operator_t != OPERATOR_T_DIV &&
+                out_dw.contiguous_memory()) {
+            cudnn_conv_bias_bwd<operator_t,T,devT>(bias_dw, out_dw, data_format);
+            return;
+        }
+#endif
+        mshadow_conv_bias_bwd<operator_t,T,devT>(bias_dw, out_dw, data_format);
     }
 
-#ifdef DALI_USE_CUDA
-    template<OPERATOR_T operator_t, typename T, DALI_FUNC_ENABLE_IF_INT>
-    void typed_eval(TypedArray<memory::DEVICE_T_GPU, T> bias_dw,
-                    TypedArray<memory::DEVICE_T_GPU, T> out_dw,
-                    const std::string& data_format) {
-        ASSERT2(false, "integer Conv2dBwdBias is not implemented for GPU.");
+
+    template<OPERATOR_T operator_t, typename T, int devT>
+    void mshadow_conv_bias_bwd(TypedArray<devT, T> bias_dw,
+                               TypedArray<devT, T> out_dw,
+                               const std::string& data_format) {
+        int c_channel = data_format.find('C');
+
+        switch(c_channel) {
+            case 0:
+                operator_assign<operator_t, 1>(
+                    bias_dw,
+                    mshadow::expr::sumall_except_dim<0>(
+                        out_dw.d4()
+                    )
+                );
+                break;
+            case 1:
+                operator_assign<operator_t, 1>(
+                    bias_dw,
+                    mshadow::expr::sumall_except_dim<1>(
+                        out_dw.d4()
+                    )
+                );
+                break;
+            case 2:
+                operator_assign<operator_t, 1>(
+                    bias_dw,
+                    mshadow::expr::sumall_except_dim<2>(
+                        out_dw.d4()
+                    )
+                );
+                break;
+            case 3:
+                operator_assign<operator_t, 1>(
+                    bias_dw,
+                    mshadow::expr::sumall_except_dim<3>(
+                        out_dw.d4()
+                    )
+                );
+                break;
+            default:
+                ASSERT2(false, utils::MS() << "data_format not understood: " << data_format);
+                break;
+        }
     }
 
-    template<OPERATOR_T operator_t, typename T, DALI_FUNC_DISABLE_IF_INT>
-    void typed_eval(TypedArray<memory::DEVICE_T_GPU, T> bias_dw,
-                    TypedArray<memory::DEVICE_T_GPU, T> out_dw,
-                    const std::string& data_format) {
+#ifdef DALI_USE_CUDNN
+    template<OPERATOR_T operator_t, typename T, int devT, DALI_FUNC_DISABLE_IF_INT>
+    void cudnn_conv_bias_bwd(TypedArray<devT, T> bias_dw,
+                             TypedArray<devT, T> out_dw,
+                             const std::string& data_format) {
 
         auto out_access_mode = internal::OperatorAM<operator_t>::get(bias_dw);
 

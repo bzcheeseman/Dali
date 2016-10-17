b3e32c056e2dd178f1c48e91e7ef5c1e1addb797
Szymon Sidor <szymon.sidor@gmail.com>
2016-10-16 17:41:42 -0700

    split unary into separate tests

--- dali/array/op2/unary_tests.cpp
+++ dali/array/op2/unary_tests.cpp
@@ -31,13 +31,16 @@
     }
 
 #define DALI_RTC_UNARY_TEST(funcname)\
-    for (auto dtype : {DTYPE_INT32, DTYPE_FLOAT, DTYPE_DOUBLE}) {\
-        int size = 10;\
-        auto a = Array::arange({5, size}, dtype) + 1;\
-        auto dst = Array::zeros({5, size}, dtype);\
-        dst = op::funcname(a);\
-        EXPECT_TRUE(Array::allclose(dst, reference_ ##funcname(a), 1e-6));\
-    }\
+    TEST(RTCTests, unary_ ##funcname) {\
+        for (auto dtype : {DTYPE_INT32, DTYPE_FLOAT, DTYPE_DOUBLE}) {\
+            int size = 10;\
+            auto a = Array::arange({5, size}, dtype) + 1;\
+            auto dst = Array::zeros({5, size}, dtype);\
+            dst = op::funcname(a);\
+            EXPECT_TRUE(Array::allclose(dst, reference_ ##funcname(a), 1e-6));\
+        }\
+    }
+
 
 DALI_DEFINE_REFERENCE_UNARY(softplus, softplus);
 DALI_DEFINE_REFERENCE_UNARY(sigmoid, sigmoid);
@@ -53,20 +56,18 @@ DALI_DEFINE_REFERENCE_UNARY(sign, sign);
 DALI_DEFINE_REFERENCE_UNARY(identity, identity);
 
 
-TEST(RTCTests, unary) {
-    DALI_RTC_UNARY_TEST(softplus);
-    DALI_RTC_UNARY_TEST(sigmoid);
-    DALI_RTC_UNARY_TEST(tanh);
-    DALI_RTC_UNARY_TEST(log);
-    DALI_RTC_UNARY_TEST(cube);
-    DALI_RTC_UNARY_TEST(sqrt);
-    DALI_RTC_UNARY_TEST(rsqrt);
-    DALI_RTC_UNARY_TEST(eltinv);
-    DALI_RTC_UNARY_TEST(relu);
-    DALI_RTC_UNARY_TEST(abs);
-    DALI_RTC_UNARY_TEST(sign);
-    DALI_RTC_UNARY_TEST(identity);
-}
+DALI_RTC_UNARY_TEST(softplus);
+DALI_RTC_UNARY_TEST(sigmoid);
+DALI_RTC_UNARY_TEST(tanh);
+DALI_RTC_UNARY_TEST(log);
+DALI_RTC_UNARY_TEST(cube);
+DALI_RTC_UNARY_TEST(sqrt);
+DALI_RTC_UNARY_TEST(rsqrt);
+DALI_RTC_UNARY_TEST(eltinv);
+DALI_RTC_UNARY_TEST(relu);
+DALI_RTC_UNARY_TEST(abs);
+DALI_RTC_UNARY_TEST(sign);
+DALI_RTC_UNARY_TEST(identity);
 
 #define DALI_DEFINE_REFERENCE_UNARY_SCALAR(FUNCNAME, FUNCTOR_NAME)\
     Array reference_scalar_ ##FUNCNAME (Array x, double scalar) {\
@@ -90,13 +91,15 @@ TEST(RTCTests, unary) {
     }
 
 #define DALI_RTC_SCALAR_UNARY_TEST(funcname)\
-    for (auto dtype : {DTYPE_INT32, DTYPE_FLOAT, DTYPE_DOUBLE}) {\
-        int size = 10;\
-        auto a = Array::arange({5, size}, dtype) + 1;\
-        auto dst = Array::zeros({5, size}, dtype);\
-        dst = op::funcname(a, 2.0);\
-        EXPECT_TRUE(Array::allclose(dst, reference_scalar_ ##funcname(a, 2.0), 1e-5));\
-    }\
+    TEST(RTCTests, scalar_unary_ ##funcname) {\
+        for (auto dtype : {DTYPE_INT32, DTYPE_FLOAT, DTYPE_DOUBLE}) {\
+            int size = 10;\
+            auto a = Array::arange({5, size}, dtype) + 1;\
+            auto dst = Array::zeros({5, size}, dtype);\
+            dst = op::funcname(a, 2.0);\
+            EXPECT_TRUE(Array::allclose(dst, reference_scalar_ ##funcname(a, 2.0), 1e-5));\
+        }\
+    }
 
 DALI_DEFINE_REFERENCE_UNARY_SCALAR(add, add);
 DALI_DEFINE_REFERENCE_UNARY_SCALAR(sub, sub);
@@ -104,10 +107,8 @@ DALI_DEFINE_REFERENCE_UNARY_SCALAR(eltmul, eltmul);
 DALI_DEFINE_REFERENCE_UNARY_SCALAR(eltdiv, eltdiv);
 DALI_DEFINE_REFERENCE_UNARY_SCALAR(pow, power);
 
-TEST(RTCTests, scalar_unary) {
-    DALI_RTC_SCALAR_UNARY_TEST(add);
-    DALI_RTC_SCALAR_UNARY_TEST(sub);
-    DALI_RTC_SCALAR_UNARY_TEST(eltmul);
-    DALI_RTC_SCALAR_UNARY_TEST(eltdiv);
-    DALI_RTC_SCALAR_UNARY_TEST(pow);
-}
+DALI_RTC_SCALAR_UNARY_TEST(add);
+DALI_RTC_SCALAR_UNARY_TEST(sub);
+DALI_RTC_SCALAR_UNARY_TEST(eltmul);
+DALI_RTC_SCALAR_UNARY_TEST(eltdiv);
+DALI_RTC_SCALAR_UNARY_TEST(pow);

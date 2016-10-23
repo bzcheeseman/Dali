01a8219cc193f97c426bab1dcf3235d863688a4a
Szymon Sidor <szymon.sidor@gmail.com>
2016-10-23 16:26:57 -0700

    removed mat folder

--- dali/mat/__MatMacros__.h
+++
@@ -1,9 +0,0 @@
-#ifndef DALI_MAT_MATH___MAT_MACROS___H
-#define DALI_MAT_MATH___MAT_MACROS___H
-
-#define MAT(matrix) ((matrix).w())
-#define GRAD(X) ((X).dw())
-
-#define SAFE_GRAD(X) if (!(X).constant) GRAD(X)
-
-#endif

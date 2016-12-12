9b45a299dd02b7c5f4d6319d72b17a879ff01686
Szymon Sidor <szymon.sidor@gmail.com>
2016-12-11 16:05:20 -0800

    ensure array can be compiled independently

--- dali/array/function2/compiler.cpp
+++ dali/array/function2/compiler.cpp
@@ -189,7 +189,7 @@ bool Compiler::compile_code(const std::string& source,
 
 std::vector<std::string> kHeaders = {
     "dali/array/function2/array_view.h",
-    "dali/array/reducer_kernels.h",
+    "dali/array/function2/reducer_kernels.h",
     "dali/array/functor.h"
 };
 #ifdef DALI_CACHE_DIR

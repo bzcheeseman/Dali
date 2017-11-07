ec7a290f37b072859303afab0e398c6e0361a4e2
Jonathan Raiman <jonathanraiman@gmail.com>
2017-11-06 23:09:25 -0800

    clarify errors + improve calling method for all_equals

--- dali/array/op/unary.cpp
+++ dali/array/op/unary.cpp
@@ -2,18 +2,18 @@
 
 namespace op {
     Array identity(Array x) {
-        throw std::runtime_error("not supported yet");
+        throw std::runtime_error("identity<Array> not supported yet");
     }
 
     Array identity(float x) {
-        throw std::runtime_error("not supported yet");
+        throw std::runtime_error("identity<float> not supported yet");
     }
 
     Array identity(double x) {
-        throw std::runtime_error("not supported yet");
+        throw std::runtime_error("identity<double> not supported yet");
     }
 
     Array identity(int x) {
-        throw std::runtime_error("not supported yet");
+        throw std::runtime_error("identity<int> not supported yet");
     }
 }
